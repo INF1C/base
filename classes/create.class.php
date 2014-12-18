f704735de1a5e94a91f2a15bada567fc20cc9d42
Pascal Drewes <pascal@drewes-webdesign.nl>
2014-12-18 15:55:36 +0100

    Combined

--- classes/create.class.php
+++ classes/create.class.php
@@ -1,132 +1,231 @@
 <?php
-require_once 'user.class.php';
-class create extends user {
-    
+
+class create {
+
+    public $db;
+    public $user;
+
     public function __construct() {
-        parent::__construct();
+        $this->db = new db();
+        $this->user = new user();
     }
     
-public function FAQ() {
-//Kevin Bosman
-$this->db_table = "MEDEWERKER";
+    /*
+     * Author: Kevin Bosman
+     */
+    
+    public function FAQ() {
+        $this->db->db_table = "MEDEWERKER";
 
-$fields = array("idMedewerker");
-$where = array("Gebruikersnaam" => $_SESSION['gebruikersnaam']);
+        $fields = array("idMedewerker");
+        $where = array("Gebruikersnaam" => $_SESSION['gebruikersnaam']);
 
-$idMedewerker = $this->select($fields, $where); 
-//↑Medewerker id van de ingelogde gebruiker
+        $idMedewerker = $this->db->select($fields, $where);
+        //↑Medewerker id van de ingelogde gebruiker
 
 
-$this->db_table = "FAQ";
+        $this->db->db_table = "FAQ";
+        $fields = array(
+            "Vraag",
+            "Beschrijving",
+            "Oplossing");
+        //↑Ophalen van de benodige velden
 
- $fields = array(
-	"Vraag",
-	"Beschrijving",
-	"Oplossing");
-	
-//↑Ophalen van de benodige velden
-
-	$data = array();
+        $data = array();
         foreach ($fields as $key) {
             $data[$key] = filter_input(INPUT_POST, $key);
-				if ($data[$key] === '') {
-					trigger_error("U heeft niets ingevuld!");
-				}
-		}
-//↑Vullen van de benodige velden
-			
-	$check = $this->insert($data);
-        if ($check === 1){
+            if ($data[$key] === '') {
+                trigger_error("U heeft niets ingevuld!");
+            }
+        }
+        //↑Vullen van de benodige velden
+
+        $check = $this->db->insert($data);
+        if ($check === 1) {
             return TRUE;
-        } 	else{
-				trigger_error("Fout tijdens het aanmaken van uw FAQ");
-			}
-//↑Velden naar database sturen
-}
+        } else {
+            trigger_error("Fout tijdens het aanmaken van uw FAQ");
+        }
+        //↑Velden naar database sturen
+    }
 
+    public function ticket() {
+        $this->db->db_table = "TICKET";
 
+        $fields = array(
+            "IncidentType",
+            "Probleemstelling",
+            "Oplossing");
+        //↑Ophalen van de benodige velden
 
-public function ticket() {
-//Kevin Bosman
-$this->db_table = "TICKET";
-
-$fields = array(
-	"IncidentType",
-	"Probleemstelling",
-	"Oplossing");
-	
-//↑Ophalen van de benodige velden
-
-	$data = array();
+        $data = array();
         foreach ($fields as $key) {
             $data[$key] = filter_input(INPUT_POST, $key);
-				if ($data[$key] === '') {
-					trigger_error("U heeft niets ingevuld!");
-				}
-		}
-		$incident = array('Vraag','Wens','Uitval','Functioneel probleem','Technisch probleem');
-			if(!in_array($data["IncidentType"], $incident)){
-				trigger_error("De invulmogelijkheden zijn: Vraag, Wens, Uitval, Functioneel probleem, Technisch probleem. Uw ingevulde incidenttype voldoet hier niet aan!");
-			}
-		
-//↑Vullen van de benodige velden
-			
-	$check = $this->insert($data);
-        if ($check === 1){
+            if ($data[$key] === '') {
+                trigger_error("U heeft niets ingevuld!");
+            }
+        }
+        $incident = array('Vraag', 'Wens', 'Uitval', 'Functioneel probleem', 'Technisch probleem');
+        if (!in_array($data["IncidentType"], $incident)) {
+            trigger_error("De invulmogelijkheden zijn: Vraag, Wens, Uitval, Functioneel probleem, Technisch probleem. Uw ingevulde incidenttype voldoet hier niet aan!");
+        }
+        //↑Vullen van de benodige velden
+
+        $check = $this->db->insert($data);
+        if ($check === 1) {
             return TRUE;
-        } 	else{
-				trigger_error("Fout tijdens het aanmaken van uw ticket");
-			}
-//↑Velden naar database sturen
+        } else {
+            trigger_error("Fout tijdens het aanmaken van uw ticket");
+        }
+        //↑Velden naar database sturen
+    }
 
-}
+    public function status($idTicket, $idBedrijfsmedewerker, $idBedrijf, $idMedewerker) {
+        $this->db->db_table = "STATUS_WIJZIGING";
 
+        $fields = array(
+            "Status",
+            "SoortContact",
+            "Memo",
+        );
+        //↑Ophalen benodige velden
 
-
-public function status($idTicket, $idBedrijfsmedewerker, $idBedrijf, $idMedewerker) {
-//Kevin Bosman
-$this->db_table = "STATUS_WIJZIGING";
-
- $fields = array(
-	"Status",
-	"SoortContact",
-	"Memo",
- );
-//↑Ophalen benodige velden
-
-	$data = array();
+        $data = array();
         foreach ($fields as $key) {
             $data[$key] = filter_input(INPUT_POST, $key);
-				if ($data[$key] === '') {
-					trigger_error("U heeft niets ingevuld!");
-				}
-		}
-//↑Checken of er iets ingevuld is	
-		
-$data["idTicket"] = $idTicket;
-$data["idBedrijfsmedewerker"] = $idBedrijfsmedewerker;
-$data["idBedrijf"] = $idBedrijf;
-$data["idMedewerker"] = $idMedewerker;
+            if ($data[$key] === '') {
+                trigger_error("U heeft niets ingevuld!");
+            }
+        }
+        //↑Checken of er iets ingevuld is	
 
-	$status = array('Nieuw','In behandeling','Doorgestuurd naar engineer','Doorgestuurd naar account manager','Opgelost','Afgemeld');
-			if(!in_array($data["Status"], $status)){
-				trigger_error("De invulmogelijkheden zijn: Nieuw, In behandeling, Doorgestuurd naar engineer, Doorgestuurd naar account manager, Opgelost , Afgemeld. Uw ingevulde incidenttype voldoet hier niet aan!");
-			}
-//↑Checken of status voldoet aan de voorwaarden		
-			
-			
-	date_default_timezone_set('Europe/Amsterdam');		
-	$date = date('Y-m-d H-i-s');
-	$data["DatumTijd"] = array("DatumTijd" => $date);
-//↑Timestamp aanmaken voor DatumTijd veld	
-	
-	$check = $this->insert($data);
-        if ($check === 1){
+        $data["idTicket"] = $idTicket;
+        $data["idBedrijfsmedewerker"] = $idBedrijfsmedewerker;
+        $data["idBedrijf"] = $idBedrijf;
+        $data["idMedewerker"] = $idMedewerker;
+
+        $status = array('Nieuw', 'In behandeling', 'Doorgestuurd naar engineer', 'Doorgestuurd naar account manager', 'Opgelost', 'Afgemeld');
+        if (!in_array($data["Status"], $status)) {
+            trigger_error("De invulmogelijkheden zijn: Nieuw, In behandeling, Doorgestuurd naar engineer, Doorgestuurd naar account manager, Opgelost , Afgemeld. Uw ingevulde incidenttype voldoet hier niet aan!");
+        }
+        //↑Checken of status voldoet aan de voorwaarden		
+
+
+        date_default_timezone_set('Europe/Amsterdam');
+        $date = date('Y-m-d H-i-s');
+        $data["DatumTijd"] = array("DatumTijd" => $date);
+        //↑Timestamp aanmaken voor DatumTijd veld	
+
+        $check = $this->db->insert($data);
+        if ($check === 1) {
             return TRUE;
-        } 	else{
-				trigger_error("Fout tijdens het wijzigigen van de status");
-			}
-//↑Velden naar database sturen			
-}
+        } else {
+            trigger_error("Fout tijdens het wijzigigen van de status");
+        }
+        //↑Velden naar database sturen			
+    }
+
+    /*
+     * Author: Martijn Posthuma
+     */
+    
+    //Dit haalt de data uit de velden en stopt ze in de database
+    public function medewerker() {
+        $this->db->db_table = "MEDEWERKER";
+        if ($this->user->register(array("Gebruikersnaam", "Wachtwoord", "Autorisatie"), "Medewerker")) {
+            $data = array();
+            $fields = array(
+                "idMedewerker",
+                "Email",
+                "voornaam",
+                "Achternaam",
+                "Tussenvoegsel");
+            foreach ($fields as $field) {
+                $data[$field] = filter_input(INPUT_POST, $field);
+                if ($data[$field] === '') {
+                    trigger_error("Nog niet alle velden zijn ingevuld");
+                }
+            }
+        }
+        // Insert the data into the database
+        $this->db->db_table = "MEDEWERKER";
+        $check = $this->insert($data);
+        if ($check === 1) {
+            return TRUE;
+        } else {
+            trigger_error("Error bij het aanmaken van uw account");
+        }
+    }
+
+    //Dit haalt de data uit de velden en stopt ze in de database
+    public function bedrijfsMedewerker($idBedrijf) {
+        $this->db->db_table = "BEDRIJF";
+        //Dit contoleerd of er wel een bedrijf is
+        $check = $this->db->select(array($idBedrijf), array("idBedrijf" => $idBedrijf));
+        if (count($check) === 1) {
+            if ($this->user->register(array("Gebruikersnaam", "Wachtwoord", "Autorisatie"), "bedrijfsMedewerker")) {
+                $data = array();
+                $fields = array(
+                    "idBedrijfsMedewerker",
+                    "idBedrijf",
+                    "Email",
+                    "voornaam",
+                    "Achternaam",
+                    "Tussenvoegsel",
+                    "Functie");
+                foreach ($fields as $field) {
+                    $data[$field] = filter_input(INPUT_POST, $field);
+                    if ($data[$field] === '') {
+                        trigger_error("Nog niet alle velden zijn ingevuld");
+                    }
+                }
+            }
+            // Insert the data into the database
+            $this->db->db_table = "BEDRIJFSMEDEWERKER";
+            $check = $this->insert($data);
+            if ($check === 1) {
+                return TRUE;
+            } else {
+                trigger_error("Error bij het aanmaken van uw account");
+            }
+        } else {
+            echo "Bedrijf bestaat niet";
+        }
+    }
+
+    //Dit haalt de data uit de velden en stopt ze in de database
+    public function bedrijf($idBedrijf) {
+        $this->db->db_table = "BEDRIJF";
+        //Dit contoleerd of er wel een bedrijf is
+        $check = $this->db->select(array($idBedrijf), array("idBedrijf" => $idBedrijf));
+        if (count($check) === 1) {
+            if ($this->user->register(array("Gebruikersnaam", "Wachtwoord", "Autorisatie"), "bedrijfsMedewerker")) {
+                $data = array();
+                $fields = array(
+                    "idBedrijf",
+                    "Bedrijfsnaam",
+                    "Adresgegevens",
+                    "Telefoon",
+                    "Email",
+                    "Licentie");
+                foreach ($fields as $field) {
+                    $data[$field] = filter_input(INPUT_POST, $field);
+                    if ($data[$field] === '') {
+                        trigger_error("Nog niet alle velden zijn ingevuld");
+                    }
+                }
+            }
+            // Insert the data into the database
+            $this->db->db_table = "BEDRIJF";
+            $check = $this->insert($data);
+            if ($check === 1) {
+                return TRUE;
+            } else {
+                trigger_error("Error bij het aanmaken van uw account");
+            }
+        } else {
+            echo "Bedrijf bestaat niet";
+        }
+    }
 
 }
