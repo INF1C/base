ca09bdcf7e35f5e782c495aeb3c45e68b80cc512
Pascal Drewes <pascal@drewes-webdesign.nl>
2014-12-19 10:38:39 +0100

    combined

--- classes/edit.class.php
+++ classes/edit.class.php
@@ -1,4 +1,5 @@
 <?php
+
 class edit {
 
     public $db;
@@ -10,7 +11,7 @@ class edit {
     /*
      * Author: Sieger van Iest
      */
-    
+
     // WIJZIG GEGEVENS MEDEWERKER
     public function medewerker($idMedewerker) {
         //GEGEVENS TERUG VOEREN
@@ -27,7 +28,7 @@ class edit {
                 trigger_error("Lege input");
             }
         }
-        
+
         $where = array("idMedewerker" => $idMedewerker);
 
         return $this->db->update($data, $where);
@@ -42,7 +43,7 @@ class edit {
             "Achternaam",
             "Tussenvoegsel",
             "Functie");
-        
+
         foreach ($fields as $key) {
             $data[$key] = filter_input(INPUT_POST, $key);
             if ($data[$key] === '') {
@@ -63,7 +64,7 @@ class edit {
             "Telefoon",
             "Email",
             "Licentie");
-        
+
         foreach ($fields as $key) {
             $data[$key] = filter_input(INPUT_POST, $key);
             if ($data[$key] === '') {
@@ -74,6 +75,81 @@ class edit {
         $where = array("idBedrijf" => $idBedrijf);
 
         return $this->db->update($data, $where);
+    }
+
+    /*
+     * Author: Rene Cordes
+     */
+
+    public function FAQ($Vraag, $idMedewerker) {
+        //GEGEVENS TERUG VOEREN
+        $fields = array(
+            "Vraag",
+            "Beschrijving",
+            "Oplossing",
+        );
+
+        foreach ($fields as $key) {
+            $data[$key] = filter_input(INPUT_POST, $key);
+            if ($data[$key] === '') {
+                trigger_error("Lege input");
+            }
+        }
+        $where = array("idMedewerker" => $idMedewerker,
+            "Vraag" => $Vraag);
+
+        return $this->db->update($data, $where);
+    }
+
+    public function Ticket($idTicket) {   //GEGEVENS TERUG VOEREN
+        $fields = array(
+            "IncidentType",
+            "Probleemstelling",
+            "Oplossing",
+        );
+
+        foreach ($fields as $key) {
+            $data[$key] = filter_input(INPUT_POST, $key);
+            if ($data[$key] === '') {
+                trigger_error("Lege input");
+            }
+        }
+
+        $EnumCheck = array("Vraag", "Wens", "Uitval", "Functioneel probleem", "Technisch probleem");
+
+        if (!in_array($data['IncidentType'], $EnumCheck))
+            die("Check failed");
+
+
+        $where = array("idTicket" => $idTicket);
+
+        return $this->db->update($data, $where);
+    }
+
+    public function StatusWijziging($idStatus) { //GEGEVENS TERUG VOEREN
+        $fields = array(
+            "idBedrijfsMedewerker",
+            "idMedewerker",
+            "Status",
+            "SoortContact",
+            "Memo",
+        );
+
+        foreach ($fields as $key) {
+            $data[$key] = filter_input(INPUT_POST, $key);
+            if ($data[$key] === '') {
+                trigger_error("Lege input");
+            }
+        }
+
+        $EnumCheck = array("Nieuw", "In behandeling", "Doorgestuurd naar engineer", "Doorgestuurd naar account manager", "opgelost", "afgemeld");
+
+        if (!in_array($data['Status'], $EnumCheck))
+            die("Check failed");
+
+        $where = array("idStatus" => $idStatus);
+
+        return $this->update($data, $where);
     }
 
 }
