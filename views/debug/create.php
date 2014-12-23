708ac7f4772edf598ebdaf03dd4eb36fb9ce8aab
Pascal Drewes <pascal@drewes-webdesign.nl>
2014-12-23 01:58:35 +0100

    HTML form create.class->medewerker

--- views/debug/create.php
+++ views/debug/create.php
@@ -1,6 +1,18 @@
+<!-- Formulier voor create.class->medewerker -->
 <div class="form">
     <form method="POST" action="">
-        <p>Gebruikersnaam: <input type="text" name="Gebruikersnaam" />
+        <p>
+        	Gebruikersnaam: <input type="text" name="Gebruikersnaam" required><br>
+       		Wachtwoord: <input type="password" name="Wachtwoord" required>
+       		E-mail: <input type="email" name="Email" required><br>
+       	</p>
+       	<hr>
+       	<p>
+       		Voornaam: <input type="text" name="Voornaam" required><br>
+       		Achternaam: <input type="text" name="Achternaam" required><br>
+    	   	Tussenvoegsel: <input type="text" name="Tussenvoegsel" required>
+       	</p>
+       	<input type="submit" name="Submit" value="Maak medewerker aan!">
     </form>
 </div>
 
