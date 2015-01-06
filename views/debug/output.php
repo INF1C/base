5d389f69ec220d2e9977faf5dfe53438c753ccd9
Rene Cordes <renecordes@hotmail.com>
2015-01-06 14:46:36 +0100

    output html test
    test

--- views/debug/output.php
+++ views/debug/output.php
@@ -7,7 +7,20 @@ $output = new output;
 foreach ($ticket as $key => $value) {
     echo "$value <br/>";
     echo "$key";
+    
 }
-echo "<pre>";
-var_dump($ticket);
-echo "</pre>";
+?>
+
+<table style="width:100%">
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td> 
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td> 
+    <td>94</td>
+  </tr>
+</table>
+
