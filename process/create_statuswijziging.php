b19d6178a0d4b1808f15720c66fc321de092290c
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-06 16:19:42 +0100

    'Whoops i did it again'

--- process/create_statuswijziging.php
+++ process/create_statuswijziging.php
@@ -7,11 +7,11 @@ if(isset($_POST['submit']) && $_POST['submit'] == 'submit'){
 	$idTicket = filter_input(INPUT_POST, 'idTicket');
 	$idBedrijfsmedewerker = filter_input(INPUT_POST, 'idBedrijfsmedewerker');
 	$db->db_table = "BEDRIJFSMEDEWERKER";
-	$idBedrijf = $db->select(array('idBedrijf'), array('idBedrijfsMedewerker' => $idBedrijfsmedewerker))['idBedrijf'];
+	$idBedrijf = $db->select(array('idBedrijf'), array('idBedrijfsMedewerker' => $idBedrijfsmedewerker))[0]['idBedrijf'];
 	echo "<pre>";
 	var_dump($idBedrijf);
 	var_dump($create->statusWijziging($idTicket, $idBedrijfsmedewerker, $idBedrijf));
-	echo "</pre>";
+	echo "</pre>"
 } else {
 	echo "Please post the next time!";
 }
