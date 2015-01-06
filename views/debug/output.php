719bf68a06cc23ac5f18a57ab978112fb1b2dc76
Rene Cordes <renecordes@hotmail.com>
2015-01-06 15:07:38 +0100

    output dit is goed
    skiere bende

--- views/debug/output.php
+++ views/debug/output.php
@@ -3,18 +3,15 @@
 $output = new output;
     
         $ticket = $output->ticket(1);
- 
+
+echo "<table style='width:100%' border='1'>";
+
 foreach ($ticket as $key => $value) {
-    echo "$value <br/>";
-    echo "$key";
-    
+    echo "<tr>";
+    echo "<td>" . $value . "</td>";
+    echo "<td>" . $key . "</td>";
+    echo "</tr>";
 }
 ?>
-
-<table style="width:100%" border="1">
-  <tr>
-    <td><?php echo "$key" ?></td>
-    <td><?php echo "$value" ?></td> 
-  </tr>
 </table>
 
