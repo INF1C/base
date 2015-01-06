10bb2c89466f690b98a2d15c627e2c05c746d3a1
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-06 16:15:34 +0100

    array to string conversion

--- process/create_statuswijziging.php
+++ process/create_statuswijziging.php
@@ -7,7 +7,7 @@ if(isset($_POST['submit']) && $_POST['submit'] == 'submit'){
 	$idTicket = filter_input(INPUT_POST, 'idTicket');
 	$idBedrijfsmedewerker = filter_input(INPUT_POST, 'idBedrijfsmedewerker');
 	$db->db_table = "BEDRIJFSMEDEWERKER";
-	$idBedrijf = $db->select(array('idBedrijf'), array('idBedrijfsMedewerker' => $idBedrijfsmedewerker));
+	$idBedrijf = $db->select(array('idBedrijf'), array('idBedrijfsMedewerker' => $idBedrijfsmedewerker))[0];
 	var_dump($create->statusWijziging($idTicket, $idBedrijfsmedewerker, $idBedrijf));
 } else {
 	echo "Please post the next time!";
