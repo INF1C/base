23c7b17b30eb411e0770021493133659094144d7
martijn13795 <martijn13795@hotmail.com>
2015-01-08 12:04:35 +0100

    index.php links veranders

--- views/thema/index.php
+++ views/thema/index.php
@@ -10,16 +10,16 @@
     <title>Stenden E-Help</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="/base/templates/subCss/bootstrap.css" rel="stylesheet">
     <!--external css-->
-    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
-    <link rel="stylesheet" type="text/css" href="assets/css/zabuto_calendar.css">
-    <link rel="stylesheet" type="text/css" href="assets/js/gritter/css/jquery.gritter.css" />
-    <link rel="stylesheet" type="text/css" href="assets/lineicons/style.css">    
+    <link href="/base/templates/subCss/font-awesome/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="/base/templates/subCss/zabuto_calendar.css">
+    <link rel="stylesheet" type="text/css" href="/base/templates/subCss/jquery.gritter.css" />
+    <link rel="stylesheet" type="text/css" href="/base/templates/subCss/style.css">    
     
     <!-- Custom styles for this template -->
-    <link href="assets/css/style.css" rel="stylesheet">
-    <link href="assets/css/style-responsive.css" rel="stylesheet">
+    <link href="/base/templates/subCss/style.css" rel="stylesheet">
+    <link href="/base/templates/subCss/style-responsive.css" rel="stylesheet">
 
     <script src="assets/js/chart-master/Chart.js"></script>
     
@@ -290,24 +290,24 @@
   </section>
 
     <!-- js placed at the end of the document so the pages load faster -->
-    <script src="assets/js/jquery.js"></script>
-    <script src="assets/js/jquery-1.8.3.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
-    <script src="assets/js/jquery.scrollTo.min.js"></script>
-    <script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>
-    <script src="assets/js/jquery.sparkline.js"></script>
+    <script src="/base/templates/subCss/js/jquery.js"></script>
+    <script src="/base/templates/subCss/js/jquery-1.8.3.min.js"></script>
+    <script src="/base/templates/subCss/js/bootstrap.min.js"></script>
+    <script class="include" type="text/javascript" src="/base/templates/subCss/js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="/base/templates/subCss/js/jquery.scrollTo.min.js"></script>
+    <script src="/base/templates/subCss/js/jquery.nicescroll.js" type="text/javascript"></script>
+    <script src="/base/templates/subCss/js/jquery.sparkline.js"></script>
 
 
     <!--common script for all pages-->
-    <script src="assets/js/common-scripts.js"></script>
+    <script src="/base/templates/subCss/js/common-scripts.js"></script>
     
-    <script type="text/javascript" src="assets/js/gritter/js/jquery.gritter.js"></script>
-    <script type="text/javascript" src="assets/js/gritter-conf.js"></script>
+    <script type="text/javascript" src="/base/templates/subCss/js/gritter/js/jquery.gritter.js"></script>
+    <script type="text/javascript" src="/base/templates/subCss/js/gritter-conf.js"></script>
 
     <!--script for this page-->
-    <script src="assets/js/sparkline-chart.js"></script>    
-	<script src="assets/js/zabuto_calendar.js"></script>	
+    <script src="/base/templates/subCss/js/sparkline-chart.js"></script>    
+	<script src="/base/templates/subCss/js/zabuto_calendar.js"></script>	
 	
 	
 	<script type="application/javascript">
