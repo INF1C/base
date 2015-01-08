79e8e8fdd0531d48c70d4a0d5ff51bdf60990628
rbeikes <memyselfandi105@hotmail.com>
2015-01-08 11:24:18 +0100

    search2 aangepast

--- views/search_test.php
+++ views/search_test.php
@@ -7,15 +7,16 @@ if (isset($_POST['zoekterm']))
     $zoekterm = filter_input(INPUT_POST, 'zoekterm');
     $optie = 'MEDEWERKER';
     $db->db_table = $optie;
-    ECHO $db->select(array('*'), array('Achternaam' => $zoekterm));
-    ?>
-<p>
-<form action="Test123.php" method="POST">
-    <strong>test</strong><br />
-    <p>Zoekterm: <input type="text" name="zoekterm" /></p>
-    <p>
-        <input type="reset" name="reset" value="Clear Form" />
-        <input type="submit" name="submit" value="Zoeken" />
-    </p>
-</form>
-</p>
+    echo $db->select(array('*'), array('Achternaam' => $zoekterm));
+}
+    
+echo "<p>" .
+"<form action='Test123.php' method='POST'>" .
+    "<strong>test</strong><br />" .
+    "<p>Zoekterm: <input type='text' name='zoekterm' /></p>" .
+    "<p>" .
+        "<input type='reset' name='reset' value='Clear Form' />" .
+        "<input type='submit' name='submit' value='Zoeken' />" .
+    "</p>" .
+"</form>" .
+"</p>";
