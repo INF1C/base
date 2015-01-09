0c9d7053ad1cd5491b0f34c7c357ae7a471ba27c
rbeikes <memyselfandi105@hotmail.com>
2015-01-09 10:01:01 +0100

    aanpassing 2 mw info

--- views/debug/output.php
+++ views/debug/output.php
@@ -1,14 +1,14 @@
 <?php
 
 $output = new output;
-$medewerkerinfo = $output->Medewerker(1);
+$medewerkerinfo = $output->Medewerker(1)[0];
       echo "<table style='width:100%' border='1'>";
 foreach ($medewerkerinfo as $key => $value) {
-  echo "<tr>";
-echo "<td>" . $value . "</td>";
-echo "<td>" . $key . "</td>";
-echo "</tr>";
-}
+    echo "<tr>";
+    echo "<td>" . $value . "</td>";
+    echo "<td>" . $key . "</td>";
+    echo "</tr>";
+
 echo "<pre>";var_dump($medewerkerinfo); echo "</pre>";
 //Oplostijd tickets
 //$output = new output;
