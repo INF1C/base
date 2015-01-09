2cc5ad2db6650e52b02971d827e4c7fe1fb91755
kbosman <k.bosman35@gmail.com>
2015-01-09 12:32:56 +0100

    Create Bedrijf Klaar

--- views/thema/forms/create_bedrijf.php
+++ views/thema/forms/create_bedrijf.php
@@ -1,26 +1,7 @@
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<title>Tourist Information Amsterdam</title>
-		
-		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-		
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-	</head>
-
-
-
-<body>
-
-<div class="container">
-<div class="row">
 <div class="col-md-6">
-
-
-
 <form method="POST" action="/process/create/bedrijf">
   <div class="form-group">
-	<label for="createMedewerkerUsername">Bedrijfsnaam:</label>
+	<label for="createBedrijfNaam">Bedrijfsnaam:</label>
     <input type="text" class="form-control" id="createMedewerkerUsername" placeholder="Bedrijfsnaam">
   </div>
   <div class="form-group">
@@ -52,11 +33,4 @@
   </div>
   <button type="submit" class="btn btn-default">Verzend</button>
 </form>
-
-
-
 </div>
-</div>
-</div>
-</body>
-</html>
