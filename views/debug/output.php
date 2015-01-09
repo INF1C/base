bff54793965669406ff0a1d26f10c130d0406e3f
rbeikes <memyselfandi105@hotmail.com>
2015-01-09 09:55:31 +0100

    Medewerker info test

--- views/debug/output.php
+++ views/debug/output.php
@@ -1,16 +1,29 @@
 <?php
-$output = new output;
 
-$opentickets = $output->oplostijdTickets();
-        echo "<table style='width:100%' border='1'>";
-foreach ($opentickets as $key => $value) {
-    echo "<tr>";
-    echo "<td>" . $value . "</td>";
-    echo "<td>" . $key . "</td>";
-    echo "</tr>";
+$output = new output;
+$medewerkerinfo = $output->Medewerker(1);
+      echo "<table style='width:100%' border='1'>";
+foreach ($medewerkerinfo as $key => $value) {
+  echo "<tr>";
+echo "<td>" . $value . "</td>";
+echo "<td>" . $key . "</td>";
+echo "</tr>";
 }
 echo "<pre>";var_dump($opentickets); echo "</pre>";
+//Oplostijd tickets
+//$output = new output;
 
+//$oplostijdtickets = $output->oplostijdTickets();
+  //      echo "<table style='width:100%' border='1'>";
+//foreach ($oplostijdtickets as $key => $value) {
+  //  echo "<tr>";
+    //echo "<td>" . $value . "</td>";
+    //echo "<td>" . $key . "</td>";
+    //echo "</tr>";
+//}
+//echo "<pre>";var_dump($opentickets); echo "</pre>";
+
+//Alle tickets weergeven
 //$output = new output;
     
   //      $ticket = $output->ticket(1);
