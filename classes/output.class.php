ff1d9de3ca447802bc8dfa537ebfb2168d09651a
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-09 10:52:01 +0100

    revert variable check

--- classes/output.class.php
+++ classes/output.class.php
@@ -65,6 +65,7 @@ class output {
 
             //Deze SQL moet uit STATUS_WIJZIGING GEHAALD worden voor bedrijf
             $idBedrijf = $this->db->select(NULL, NULL, "SELECT idBedrijf FROM STATUS_WIJZIGING WHERE idTicket = " . $openticket . " ORDER BY idStatus ASC LIMIT 1")[0]['idBedrijf'];
+            var_dump($idBedrijf);
             //Alle data uit BEDRIJF wordt gehaald
 
             $this->db_table = "BEDRIJF";
