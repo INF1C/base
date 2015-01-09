7b3fad438e3e548bce578697d820161da8502b0a
kbosman <k.bosman35@gmail.com>
2015-01-09 12:20:02 +0100

    Create Medewerker form klaar

--- views/thema/forms/form_test.php
+++ views/thema/forms/form_test.php
@@ -1,12 +1,31 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Tourist Information Amsterdam</title>
+		
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	</head>
+
+
+
+<body>
+
+<div class="container">
+<div class="row">
 <div class="col-md-6">
-<form method="POST" action="/process/create/medewerker">
+
+
+
+<form method="POST" action="/process/create/bedrijf">
   <div class="form-group">
 	<label for="createMedewerkerUsername">Gebruikersnaam:</label>
     <input type="text" class="form-control" id="createMedewerkerUsername" placeholder="Gebruikersnaam">
   </div>
   <div class="form-group">
-    <label for="createMedewerkerPassword">Wachtwoord:</label>
-    <input type="password" class="form-control" id="createMedewerkerPassword" placeholder="Wachtwoord">
+    <label for="createBedrijfAdresGegevens">Adresgegevens:</label>
+	<textarea class="form-control" rows="3" id="createBedrijfAdresGegevens" placeholder="Adresgegevens"></textarea>
   </div>
   <div class="form-group">
 	<label for="createMedewerkerVoornaam">Voornaam:</label>
@@ -26,4 +45,11 @@
   </div>
   <button type="submit" class="btn btn-default">Verzend</button>
 </form>
+
+
+
 </div>
+</div>
+</div>
+</body>
+</html>
