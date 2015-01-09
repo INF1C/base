7df0ce1376b0e63d6e4eb146b29aa5023999b9f6
LesleyJordan <ljvanoostenrijk@live.nl>
2015-01-09 09:17:08 +0100

    oe la la
    oe la la

--- views/thema/index.php
+++ views/thema/index.php
@@ -42,7 +42,7 @@
                   <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
               </div>
             <!--logo start-->
-            <a href="index.html" class="logo"><b>Stenden E-Help</b></a>
+            <a href="index.php" class="logo"><b>Stenden E-Help</b></a>
             <!--logo end-->
             <div class="nav notify-row" id="top_menu">
                 <!--  notification start -->
