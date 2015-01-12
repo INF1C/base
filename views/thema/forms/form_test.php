3ef456e9cf7f7ae385c1213c7582fc25d4aeafde
kbosman <k.bosman35@gmail.com>
2015-01-12 12:27:35 +0100

    create ticket werkend

--- views/thema/forms/form_test.php
+++ views/thema/forms/form_test.php
@@ -18,17 +18,17 @@
 <form method="POST" action="/process/create/faq">
     <div class="form-group">
 	<label for="createFaqVraag">Soort Contact:</label>
-    <input type="text" class="form-control" id="createFaqVraag" placeholder="Vraag">
+    <input type="text" class="form-control" id="createFaqVraag" name="Vraag" placeholder="Vraag">
   </div>
   
 <div class="form-group">
     <label for="createFaqBeschrijving">Beschrijving:</label>
-	<textarea class="form-control" rows="5" id="createFaqBeschrijving" placeholder="Beschrijving"></textarea>
+	<textarea class="form-control" rows="5" id="createFaqBeschrijving" name="Beschrijving" placeholder="Beschrijving"></textarea>
 </div>
 
 <div class="form-group">
     <label for="createFaqOplossing">Oplossing:</label>
-	<textarea class="form-control" rows="5" id="createFaqOplossing" placeholder="Oplossing"></textarea>
+	<textarea class="form-control" rows="5" id="createFaqOplossing" name="Oplossing" placeholder="Oplossing"></textarea>
 </div>
 
   <button type="submit" name="submit" value="submit" class="btn btn-default">Verzend</button>
