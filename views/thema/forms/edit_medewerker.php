983daf4fcda94d92ab336c3ab7e20000e4128365
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-12 14:14:49 +0100

    edit -> form edit medewerker

--- views/thema/forms/edit_medewerker.php
+++ views/thema/forms/edit_medewerker.php
@@ -1,3 +1,13 @@
+<?php
+$db = new db;
+if(!empty(FILTER_INPUT(INPUT_GET, 'idBedrijf'))) {
+    $idBedrijf = FILTER_INPUT(INPUT_GET, 'idBedrijf');
+    $db->db_table = "BEDRIJF";
+    $data = $db->select(array('*'), array('idBedrijf' => $idBedrijf))[0];
+} else {
+    $data = array_fill_keys(array('idBedrijf', 'Bedrijfsnaam', 'Adresgegevens', 'Telefoon', 'Email', 'Licentie'), '');
+}
+?>
 <form method="POST" action="/process/edit/medewerker">
         <div class="form-group">
             <label for="Voornaam">Voornaam:</label>
