66567031c37b764fbbbdd20107805b6503ab219e
kbosman <k.bosman35@gmail.com>
2015-01-12 14:07:45 +0100

    Search aangepast

--- views/search_test_bedrijfmw-orgineel.php
+++ views/search_test_bedrijfmw-orgineel.php
@@ -7,7 +7,7 @@ if (isset($_POST['zoekterm']))
     $zoekterm = filter_input(INPUT_POST, 'zoekterm');
     $optie = 'BEDRIJFSMEDEWERKER';
     $db->db_table = $optie;
-    echo "<table class='table table-striped'>";
+    echo "<table border='2'>";
     echo "<tr><th> Link </th>";
     echo "<th> BedrijfsID </th>";
     echo "<th> Gebruikersnaam </th>";
@@ -34,23 +34,12 @@ if (isset($_POST['zoekterm']))
 }
     
 echo "<p>" .
-"<form action='' method='POST' class='in-line'>" .
-    "<div class='form-group'>
-		<label class='sr-only' for='searchBedrijfMedewerkerZoekterm'>\</label>
-		<input type='text' class='form-control' id='searchBedrijfMedewerkerZoekterm' placeholder='Zoekterm' name='zoekterm'>
-	</div>" .
-	
-	"<div class='form-group'>
-		<label class='sr-only' for='searchBedrijfMedewerkerReset'>\</label>
-		<input type='reset' class='form-control' id='searchBedrijfMedewerkerReset' value='Reset' name='reset'>
-	</div>" .
-	
-	"<div class='form-group'>
-		<label class='sr-only' for='searchBedrijfMedewerkerSubmit'>\</label>
-		<input type='submit' class='form-control' id='searchBedrijfMedewerkerSubmit' value='Zoeken' name='submit'>
-	</div>" .
-	"</p>" .
+"<form action='' method='POST'>" .
+    "<strong>test</strong><br />" .
+    "<p>Zoekterm: <input type='text' name='zoekterm' /></p>" .
+    "<p>" .
+        "<input type='reset' name='reset' value='Clear Form' />" .
+        "<input type='submit' name='submit' value='Zoeken' />" .
+    "</p>" .
 "</form>" .
 "</p>";
-
-	
