318b38f65d842fbcfe6f060bf4736fda022f8629
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-12 12:13:45 +0100

    var_dump

--- views/debug/output.pascal.php
+++ views/debug/output.pascal.php
@@ -2,7 +2,9 @@
 
 $output = new output;
 // date('Y-m-d H-i-s')
-$result = $output->tickets(NULL, array('start' => '2015-01-01 00:00:00', 'stop' => date('Y-m-d H-i-s')));
+$timeframe = array('start' => '2015-01-01 00:00:00', 'stop' => date('Y-m-d H-i-s'));
+var_dump($timeframe);
+$result = $output->tickets(NULL, $timeframe);
 
 echo "<pre>";
 var_dump($result);
