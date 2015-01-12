6f43987f06a856e1a05e6f6f5b86c351bc22274c
kbosman <k.bosman35@gmail.com>
2015-01-12 11:49:27 +0100

    tyfus aids

--- views/thema/forms/create_ticket.php
+++ views/thema/forms/create_ticket.php
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<title>Tourist Information Amsterdam</title>
-		
-		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-		
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-	</head>
-
-
-
-<body>
-
-<div class="container">
-<div class="row">
 <div class="col-md-6">
 <form method="POST" action="/process/create/ticket">
 <div class="form-group">
@@ -60,11 +44,3 @@
   <button type="submit" value="submit" class="btn btn-default">Verzend</button>
 </form>
 </div>
-
-
-
-
-</div>
-</div>
-</body>
-</html>
