1e1aa250267d13376b3e760ed89a370ef3fe92ff
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-12 12:22:35 +0100

    cleaning code

--- views/debug/output.pascal.php
+++ views/debug/output.pascal.php
@@ -3,7 +3,6 @@
 $output = new output;
 // date('Y-m-d H-i-s')
 $timeframe = array('start' => '2015-01-01 00:00:00', 'stop' => date('Y-m-d H-i-s'));
-echo "<pre>"; var_dump($timeframe); echo "</pre>";
 $result = $output->tickets(NULL, $timeframe);
 
 echo "<pre>";
