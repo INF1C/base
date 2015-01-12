c6b9cb4eb4b6105efe4d7f455d152f82537c10d0
kbosman <k.bosman35@gmail.com>
2015-01-12 12:03:21 +0100

    create statuswijziging

--- views/thema/forms/form_test.php
+++ views/thema/forms/form_test.php
@@ -15,48 +15,36 @@
 <div class="container">
 <div class="row">
 <div class="col-md-6">
-<form method="POST" action="/process/create/ticket">
+<form method="POST" action="/process/create/statuswijziging">
 <div class="form-group">
-	<label for="createTicketType">IncidentType:</label>
-    <div class="radio">
-		<label>
-		<input type="radio" name="IncidentType" id="createTicketTypeVraag" value="Vraag" checked>
-		Vraag
-		</label>
-	</div>
-	<div class="radio">
-	<label>
-		<input type="radio" name="IncidentType" id="createTicketTypeWens" value="Wens">
-		Wens
-	</label>
-	</div>
-	<div class="radio">
-		<label>
-		<input type="radio" name="IncidentType" id="createTicketTypeUitval" value="Uitval">
-		Uitval
-		</label>
-	</div>
-	<div class="radio">
-	<label>
-		<input type="radio" name="IncidentType" id="createTicketTypeFunctioneelProbleem" value="Functioneel Probleem">
-		Wens
-	</label>
-	</div>
-	<div class="radio">
-	<label>
-		<input type="radio" name="IncidentType" id="createTicketTypeTechnischProbleem" value="Technisch Probleem">
-		Wens
-	</label>
-	</div>
-  </div> 
+	<input type="hidden" name="Status" value="Nieuw" />
+</div> 
+
 <div class="form-group">
-    <label for="createTicketProbleemstelling">Oplossing:</label>
-	<textarea class="form-control" rows="5" id="createTicketProbleemstelling" placeholder="Probleemstelling" resize="none"></textarea>
-</div>
- <div class="form-group">
-    <label for="createTicketOplossing">Oplossing:</label>
-	<textarea class="form-control" rows="5" id="createTicketOplossing" placeholder="Oplossing" resize="none"></textarea>
+	<label for="createStatusWijzigingTicketID">Ticket ID:</label>
+    <input type="number" class="form-control" id="createStatusWijzigingTicketID">
   </div>
+  
+ <div class="form-group">
+	<label for="createStatusWijzigingBedrijfsMedewerkerID">Bedrijfsmedewerker ID:</label>
+    <input type="number" class="form-control" id="createStatusWijzigingBedrijfsMedewerkerID">
+  </div>
+  
+   <div class="form-group">
+	<label for="createStatusWijzigingMedewerkerID">Medewerker ID:</label>
+    <input type="number" class="form-control" id="createStatusWijzigingMedewerkerID">
+  </div>
+  
+   <div class="form-group">
+	<label for="createStatusWijzigingBedrijfsContact">Soort Contact:</label>
+    <input type="text" class="form-control" id="createStatusWijzigingBedrijfsContact">
+  </div>
+  
+<div class="form-group">
+    <label for="createStatusWijzigingMemo">Memo:</label>
+	<textarea class="form-control" rows="5" id="createStatusWijzigingMemo" placeholder="Memo" resize="none"></textarea>
+</div>
+
   <button type="submit" value="submit" class="btn btn-default">Verzend</button>
 </form>
 </div>
