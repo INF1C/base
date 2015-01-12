95c525f49dfa10b982331da30391ba5fb83a3079
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-12 13:49:28 +0100

    edit -> form create statuswijziging

--- views/thema/forms/create_statuswijziging.php
+++ views/thema/forms/create_statuswijziging.php
@@ -36,34 +36,3 @@
         <button type="submit" value="submit" name="submit" class="btn btn-default">Verzend</button>
     </form>
 </div>
-
-
-	<!-- CREATE statuswijziging (werkt) -->
-<!--     <form method="POST" action="/process/create/statuswijziging">
-	    <p>
-		    <input type="hidden" name="Status" value="Nieuw" />
-	    </p>
-        <p>
-            <span>Ticket ID: (wordt normaal door gegeven via de get)</span>
-            <input type="number" name="idTicket" />
-        </p>
-        <p>
-            <span>Bedrijfsmedewerker ID: (wordt normaal opgehaald uit de sessie)</span>
-            <input type="number" name="idBedrijfsmedewerker" />
-        </p>
-        <p>
-            <span>Medewerker ID ID: (wordt normaal opgehaald uit de sessie)(Wordt alleen weergegeven als de medewerker is ingelogd)</span>
-            <input type="number" name="idMedewerker" />
-        </p>
-	    <p>
-		    <span>Soort Contact: (bijv. telefonisch</span>
-		    <input type="text" name="SoortContact" />
-	    </p>
-	    <p>
-		    <span>Memo:</span>
-		    <textarea rows="5" cols="75"></textarea>
-	    </p>
-		<p>
-	    	<input type="submit" value="submit" name="submit">
-	    </p>
-    </form>  -->
