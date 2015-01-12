e3e7092fdc35236a1945075a1ee2ad3446910d18
Rene Cordes <renecordes@hotmail.com>
2015-01-12 13:54:33 +0100

    edit_ticket form bewerkt en compleet
    jeeej

--- views/thema/forms/edit_ticket.php
+++ views/thema/forms/edit_ticket.php
@@ -1,25 +1,56 @@
+<div class="col-md-6">
 <form method="POST" action="/process/edit/ticket">
     	<div class="form-group">
-    		<span>IncidentType</span>
-    		<select name="IncidentType">
-    			<option value="Vraag" <?= $data['IncidentType'] == 'Vraag' ? 'selected' : '' ?>>Vraag</option>
-    			<option value="Wens" <?= $data['IncidentType'] == 'Wens' ? 'selected' : '' ?>>Wens</option>
-    			<option value="Uitval" <?= $data['IncidentType'] == 'Uitval' ? 'selected' : '' ?>>Uitval</option>
-    			<option value="Functioneel probleem" <?= $data['IncidentType'] == 'Functioneel probleem' ? 'selected' : '' ?>>Functioneel Probleem</option>
-    			<option value="Technisch probleem" <?= $data['IncidentType'] == 'Technisch probleem' ? 'selected' : '' ?>>Technisch Probleem</option>
-    		</select>
+    		<label for="editTicketIncidentType">IncidentType:</label>
+                
+                <div class="radio">
+		<label>
+		<input type="radio" id="editTicketIncidentTypeVraag" value="Vraag" name="IncidentType" <?= $data['IncidentType'] == 'Vraag' ? 'checked' : '' ?>>
+		Vraag
+		</label>
+                    </div>
+                
+                <div class="radio">
+		<label>
+		<input type="radio" id="editTicketIncidentTypeWens" value="Wens" name="IncidentType" <?= $data['IncidentType'] == 'Wens' ? 'checked' : '' ?>>
+		Wens
+		</label>
+                    </div>
+                
+                <div class="radio">
+		<label>
+		<input type="radio" id="editTicketIncidentTypeUitval" value="Uitval" name="IncidentType" <?= $data['IncidentType'] == 'Uitval' ? 'checked' : '' ?>>
+		Uitval
+		</label>
+                    </div>
+                
+                <div class="radio">
+		<label>
+		<input type="radio" id="editTicketIncidentTypeVraag" value="Functioneel probleem" name="IncidentType" <?= $data['IncidentType'] == 'Functioneel probleem' ? 'checked' : '' ?>>
+		Functioneel probleem
+		</label>
+                    </div>
+                
+                <div class="radio">
+		<label>
+		<input type="radio" id="editTicketIncidentTypeVraag" value="Technisch probleem" name="IncidentType" <?= $data['IncidentType'] == 'Technisch probleem' ? 'checked' : '' ?>>
+		Technisch probleem
+		</label>
+                    </div>
     	</div>
 	    <div class="form-group">
-		    <span>Probleemstelling</span>
-                    <textarea rows="5" cols="75" name="Probleemstelling"><?= $data['Probleemstelling'] ?></textarea>
+                <label for="editProbleemstelling">Probleemstelling</label>
+                    <textarea class="form-control" id="editProbleemstelling" placeholder="Probleemstelling" rows="5" cols="75" name="Probleemstelling"><?= $data['Probleemstelling'] ?></textarea>
 	    </div>
-	    <div class="form-group">
-		    <span>Oplossing</span>
-		    <textarea rows="5" cols="75" name="Oplossing"><?= $data['Oplossing'] ?></textarea>
+            
+            <div class="form-group">
+                <label for="editOplossing">Oplossing</label>
+                    <textarea class="form-control" id="editOplossing" placeholder="Oplossing" rows="5" cols="75" name="Oplossing"><?= $data['Oplossing'] ?></textarea>
 	    </div>
+      
 	    <div class="form-group">
 	    	<input type="hidden" value="<?= $idTicket ?>" name="idTicket" />
 	    	<input type="submit" value="submit" name="submit">
 	    </div>
 </form>
-
+</div>
