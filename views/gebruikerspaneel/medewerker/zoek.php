f492771004cd2d7b357b6aa5a1e771e517788e97
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-13 18:17:25 +0100

    make up

--- views/gebruikerspaneel/medewerker/zoek.php
+++ views/gebruikerspaneel/medewerker/zoek.php
@@ -1,43 +1,45 @@
-<?php
-$db = new db();
-
-if (isset($_POST['zoekterm']))
-{
-	$zoekterm = "%" . filter_input(INPUT_POST, 'zoekterm') . "%";
-	$stmt = $db->link->prepare("SELECT * FROM MEDEWERKER WHERE Voornaam LIKE ? OR Achternaam LIKE ?");
-	$stmt->bindValue(1, $zoekterm);
-	$stmt->bindValue(2, $zoekterm);
-	$stmt->execute();
-	$returnArray = $stmt->fetchAll(PDO::FETCH_ASSOC);
-	echo "<table class='table table-hover'>";
-	echo "<tr><th> Link </th>";
-	echo "<th> Gebruikersnaam </th>";
-	echo "<th> E-mail </th>";
-	echo "<th> Voornaam </th>";
-	echo "<th> Achternaam </th>";
-	echo "<th> Tussenvoegsel </th>";
-	echo "<th> Afbeelding </th>";
-	foreach($returnArray as $result)
-	{
-		echo "<tr>";
-		foreach($result as $key => $subresult){
-			echo "<td>";
-			if($key == "idMedewerker"){
-				$id = $subresult;
-			} else {
-				echo $subresult;
-			}
-			echo "</td>";
-		}
-		echo "<td><a href='/medewerker/edit/?idMedewerker=" . $id . "'>Klik hier om te bewerken</a></td>";
-		echo "</tr>";
-	}
-} else {
-	$zoekterm = "";
-}
-?>
-
 <div class="col-lg-12">
+	<div class="form-panel">
+		<?php
+		if (isset($_POST['zoekterm']))
+		{
+			$db = new db();
+			$zoekterm = "%" . filter_input(INPUT_POST, 'zoekterm') . "%";
+			$stmt = $db->link->prepare("SELECT * FROM MEDEWERKER WHERE Voornaam LIKE ? OR Achternaam LIKE ?");
+			$stmt->bindValue(1, $zoekterm);
+			$stmt->bindValue(2, $zoekterm);
+			$stmt->execute();
+			$returnArray = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			echo "<table class='table table-hover'>";
+			echo "<tr>";
+			echo "<th> Gebruikersnaam </th>";
+			echo "<th> E-mail </th>";
+			echo "<th> Voornaam </th>";
+			echo "<th> Achternaam </th>";
+			echo "<th> Tussenvoegsel </th>";
+			echo "<th> Afbeelding </th>";
+			echo "<th> Link </th>";
+			echo "</tr>";
+			foreach($returnArray as $result)
+			{
+				echo "<tr>";
+				foreach($result as $key => $subresult){
+					echo "<td>";
+					if($key == "idMedewerker"){
+						$id = $subresult;
+					} else {
+						echo $subresult;
+					}
+					echo "</td>";
+				}
+				echo "<td><a href='/medewerker/edit/?idMedewerker=" . $id . "'>Klik hier om te bewerken</a></td>";
+				echo "</tr>";
+			}
+		} else {
+			$zoekterm = "";
+		}
+		?>
+	</div><!-- /form-panel -->
 	<div class="form-panel">
 		<h4 class="mb"><i class="fa fa-angle-right"></i> Zoek medewerker</h4>
 		<form class="form-inline" method="POST" action="" role="form">
