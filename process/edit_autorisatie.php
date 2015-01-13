519cf284912ebfbfdd970d2c403763351e2719ba
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-13 21:01:47 +0100

    ; forgotten

--- process/edit_autorisatie.php
+++ process/edit_autorisatie.php
@@ -3,6 +3,6 @@ $db = new db;
 if(isset($_POST['submit']) && $_POST['submit'] == 'submit'){
 	$db->db_table = "ACCOUNT";
 	$data = array('Autorisatie' => filter_input(INPUT_POST, 'Autorisatie'));
-	$where = array('Gebruikersnaam' => filter_input(INPUT_POST, 'Gebruikersnaam'))
+	$where = array('Gebruikersnaam' => filter_input(INPUT_POST, 'Gebruikersnaam'));
 	var_dump($db->update($data, $where));
 }
