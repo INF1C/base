efca3f5f4b9e30dd39d44d21b40127e5888adc99
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-14 10:29:25 +0100

    test

--- views/rapporten/tickets.php
+++ views/rapporten/tickets.php
@@ -3,13 +3,13 @@
 		<h4 class="mb"><i class="fa fa-angle-right"></i> Selecteer periode</h4>
 		<form class="form-inline" method="POST" action="" role="form">
 			<div class="form-group">
-	    		<div class="input-append date" id="start" data-date-format="yyyy-mm-dd hh:mm:ss">
+	    		<div class="input-append date form_datetime" id="start" data-date-format="yyyy-mm-dd hh:mm:ss">
 	    			<input class="span2" name="start" type="text">
 	    			<span class="add-on"><i class="icon-th"></i></span>
 	    		</div>
     		</div>
 			<div class="form-group">
-	    		<div class="input-append date" id="stop" data-date-format="yyyy-mm-dd hh:mm:ss">
+	    		<div class="input-append date form_datetime" id="stop" data-date-format="yyyy-mm-dd hh:mm:ss">
 	    			<input class="span2" name="stop" type="text">
 	    			<span class="add-on"><i class="icon-th"></i></span>
 	    		</div>
