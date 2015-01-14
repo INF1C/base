1f23620a7e52109d7417a8b04c789aef87e1c548
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-15 00:56:41 +0100

    edit contactgegevens

--- views/klantpaneel/contactgegevensedit.php
+++ views/klantpaneel/contactgegevensedit.php
@@ -1,9 +1,36 @@
-<?php
-$db = new db;
-$Gebruikersnaam = $_SESSION['gebruikersnaam'];
-
-$idBedrijfsmedewerker = $db->select(NULL, NULL, "SELECT idBedrijfsMedewerker
-                                 FROM BEDRIJFSMEDEWERKER
-                                 WHERE Gebruikersnaam = '" . $Gebruikersnaam . "'")[0]['idBedrijfsMedewerker'];
-medewerker($idBedrijfsmedewerker);
-?>
+<div class="col-md-6 showback">
+    <?php
+        $db = new db;
+		$db->db_table = "BEDRIJFSMEDEWERKER";
+        $data = $db->select(array('*'), array('Gebruikersnaam' => $_SESSION['gebruikersnaam']))[0];
+        } else {
+            $data = array_fill_keys(array('idBedrijfsMedewerker', 'Voornaam', 'Tussenvoegsel', 'Achternaam', 'Email', 'Functie'), '');
+        }
+        ?>
+    <form method="POST" action="/process/edit/bedrijfsmedewerker">
+        <div class="form-group">
+            <label for="Voornaam">Voornaam:</label>
+            <input type="text" class="form-control" id="Voornaam" name="Voornaam" value="<?= $data['Voornaam'] ?>" placeholder="Voornaam" />
+        </div>
+        <div class="form-group">
+            <label for="Tussenvoegsel">Tussenvoegsel:</label>
+            <input type="text" class="form-control" id="Tussenvoegsel" name="Tussenvoegsel" value="<?= $data['Tussenvoegsel'] ?>" placeholder="Tussenvoegsel" />
+        </div>
+        <div class="form-group">
+            <label for="Achternaam">Achternaam:</label>
+            <input type="text" class="form-control" id="Achternaam" name="Achternaam" value="<?= $data['Achternaam'] ?>" placeholder="Achternaam"/>
+        </div>
+        <div class="form-group">
+            <label for="Functie">Functie:</label>
+            <input type="text" class="form-control" id="Functie" name="Functie" value="<?= $data['Functie'] ?>" placeholder="Functie" />
+        </div>
+        <div class="form-group">
+            <label for="Email">E-mail:</label>
+            <input type="email" class="form-control" id="Email" name="Email" value="<?= $data['Email'] ?>" placeholder="E-mail" />
+        </div>
+        <div class="form-group">
+            <input type="hidden" value="<?= $data['idBedrijfsMedewerker'] ?>" name="idBedrijfsMedewerker">
+            <input type="submit" value="submit" name="submit" />
+        </div>
+    </form>
+</div>
