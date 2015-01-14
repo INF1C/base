096c3f72eb19a9cdfe71a7b1d77a1421b47f445a
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-14 19:26:11 +0100

    create faq

--- process/create_faq.php
+++ process/create_faq.php
@@ -1,14 +1,8 @@
 <?php
 
 $create = new create;
-$db = new db;
-
 if(isset($_POST['submit']) && $_POST['submit'] == 'submit'){
-	session_start();
-	$_SESSION['gebruikersnaam'] = "pascal";
-	echo "<pre>";
 	var_dump($create->faq());
-	echo "</pre>";
 } else {
 	echo "Please post the next time!";
 }
