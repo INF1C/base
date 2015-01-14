25a29121835a123821a78419f60324851ba39ba4
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-14 22:53:26 +0100

    single ticket

--- views/ticket/view_post.php
+++ views/ticket/view_post.php
@@ -4,25 +4,40 @@ if(isset($_POST['idTicket'])){
 	$output = new output;
 	$ticket = $output->ticket($idTicket);
 	$status = $output->Statuswijziging($idTicket);
-	// $laatsteStatus = array_pop($status);
+	$laatsteStatus = array_pop($status);
 
-echo "<pre>"; var_dump(get_defined_vars()); echo "</pre>";
+	?>
+	<div class="col-lg-8 showback">
+		<table class="table">
+			<tr>
+				<td>Ticket ID:</td>
+				<td><?= $idTicket ?></td>
+			</tr>
+			<tr>
+				<td>Bedrijf:</td>
+				<td><?= $laatsteStatus['Medewerker'] ?></td>
+			</tr>
+			<tr>
+				<td>Incident type:</td>
+				<td><?= $ticket['IncidentType'] ?></td>
+			</tr>
+			<tr>
+				<td>Probleemstelling:</td>
+				<td><?= $ticket['Probleemstelling'] ?></td>
+			</tr>
+			<tr>
+				<td>Oplossing:</td>
+				<td><?= $ticket['Oplossing'] ?></td>
+			</tr>
+		</table>
+	</div>
+	<div class="col-lg-4 showback">
+		<img class="img-responsive img-circle" src="https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-512.png" alt="Anonymous">
+		<p>Medewerker: <?= $laatsteStatus['Medewerker'] ?></p>
+	</div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	<?php
+	echo "<pre>"; var_dump(get_defined_vars()); echo "</pre>";
 
 }
 
