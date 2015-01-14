38db31ad6eb82fb5b6af52304b08c6b85ee44557
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-14 22:05:59 +0100

    rapporten -> enkele ticket

---
+++ views/rapporten/ticket.php
@@ -0,0 +1,11 @@
+<div class="col-lg-12 showback">
+		<h1>Ticket</h1>
+		<h4 class="mb"><i class="fa fa-angle-right"></i> Voer het ticket ID in</h4>
+		<form class="form-inline" method="GET" action="/ticket/" role="form">
+			<div class="form-group">
+				<label class="sr-only" for="zoekterm">TicketID:</label>
+				<input type="text" name="zoekterm" class="form-control" id="zoekterm" placeholder="0000">
+			</div>
+			<button type="submit" class="btn btn-theme">Ga</button>
+		</form>
+</div>
