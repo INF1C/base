70fe976d8d347d3ed41f4c4f69b6c6d449db5fa1
rbeikes <memyselfandi105@hotmail.com>
2015-01-14 14:13:36 +0100

    Contactgegevens edit

---
+++ views/klantpaneel/contactgegevensedit.php
@@ -0,0 +1,9 @@
+<?php
+$db = new db;
+$Gebruikersnaam = $_SESSION['gebruikersnaam'];
+
+$idBedrijfsmedewerker = $db->select(NULL, NULL, "SELECT idBedrijfsMedewerker
+                                 FROM BEDRIJFSMEDEWERKER
+                                 WHERE Gebruikersnaam = '" . $Gebruikersnaam . "'")[0]['idBedrijf'];
+medewerker($idBedrijfsmedewerker);
+?>
