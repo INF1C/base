832e2b80008ed892370c5e360fc57761026fa807
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-14 10:11:38 +0100

    test

--- views/rapporten/tickets.php
+++ views/rapporten/tickets.php
@@ -2,40 +2,16 @@
 	<div class="form-panel">
 		<h4 class="mb"><i class="fa fa-angle-right"></i> Selecteer periode</h4>
 		<form class="form-inline" method="POST" action="" role="form">
-			<!-- <div id="start" class="input-append date">
-				<label>Start:</label>
-      			<input type="text" data-format="yyyy-mm-dd HH:mm:ss"></input>
-      			<span class="add-on">
-        			<i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>
-      			</span>
-    		</div>
-    		<div id="stop" class="input-append date">
-    			<label>Stop:</label>
-      			<input type="text" data-format="yyyy-mm-dd HH:mm:ss"></input>
-      			<span class="add-on">
-        			<i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>
-      			</span>
-    		</div> -->
     		<div class="input-append date" id="start" data-date-format="yyyy-mm-dd hh:mm:ss">
-    			<input class="span2" name="start" size="16" type="text">
+    			<input class="span2" name="start" type="text">
     			<span class="add-on"><i class="icon-th"></i></span>
     		</div>
     		<div class="input-append date" id="stop" data-date-format="yyyy-mm-dd hh:mm:ss">
-    			<input class="span2" name="stop" size="16" type="text">
+    			<input class="span2" name="stop" type="text">
     			<span class="add-on"><i class="icon-th"></i></span>
     		</div>
 			<button type="submit" class="btn btn-theme">Zoek</button>
 		</form>
-	<!--	<script type="text/javascript">
-			$(function () {
-				$('#start').datetimepicker({
-					pickTime: true
-				});
-				$('#stop').datetimepicker({
-					pickTime: true
-				});
-			});
-		</script> -->
 	</div><!-- /form-panel -->
 	<?php
 
