4fa872087d4d0c16d97010ef09cbc41c6a8a4070
rbeikes <memyselfandi105@hotmail.com>
2015-01-14 10:08:49 +0100

    FAQ test

---
+++ views/gebruikerspaneel/faq.php
@@ -0,0 +1,12 @@
+<?php
+$output = new output;
+$faqinfo = $output->FaqOphalen();
+                      echo "<table style='width:100%' border='1'>";
+foreach ($faqinfo as $key => $value) {
+    echo "<tr>";
+    echo "<td>" . $key . "</td>";
+    echo "<td>" . $value . "</td>";
+    echo "</tr>";
+}
+echo "<pre>";var_dump($faqinfo); echo "</pre>";
+?>
