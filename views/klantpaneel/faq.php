0af3b07cecdb6469de565fdb530cb1e64e3dce0b
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-15 00:20:38 +0100

    faq

--- views/klantpaneel/faq.php
+++ views/klantpaneel/faq.php
@@ -1,28 +1,30 @@
-<div class="col-lg-12">
+<div class="col-lg-12 showback">
+<h2>FAQ - Frequently Asked Questions</h2>
 <?php
 $output = new output;
 $faqinfo = $output->FAQ();
-?>
-<div class="content-panel">
-		<table class="table table-hover">
-                    <tr>
-                        <th>Vraag</th>
-                        <th>Beschrijving</th>
-                        <th>Oplossing</th>
-                        <th>ID Medewerker</th>
-                    </tr>
-                    
-                    <?php
-foreach ($faqinfo as $key => $value) {
+echo "<table class='table table-hover'>";
+echo "<tr>";
+echo "<th> Vraag </th>";
+echo "<th> Beschrijving </th>";
+echo "<th> Oplossing </ht>";
+echo "<th> Auteur </th>";
+echo "</tr>";
+$output->db->db_table = "MEDEWERKER";
+foreach ($faqinfo as $value) {
     echo "<tr>";
-    foreach ($value as $faq)
-    {
-    echo "<td>" . $faq;
-    echo "</td>";
+    foreach ($value as $key => $subvalue) {
+        if($key == "idFAQ"){
+            $idFAQ = $subvalue;
+        } elseif($key == "idMedewerker") {
+            continue;
+        } 
+        echo "<td>" . $subvalue . "</td>";
     }
+    echo "<td>" . implode(' ', $output->db->select(array('Voornaam', 'Achternaam'), array('idMedewerker' => $idMedewerker))[0]) . "</td>";
     echo "</tr>";
 }
+echo "</table>";
 ?>
-</table>
 </div>
 </div>
