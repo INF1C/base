7eec2e9a0d8eb7d6538f72b34c7f937427474bc3
rbeikes <memyselfandi105@hotmail.com>
2015-01-14 10:45:28 +0100

    FAQ aangepast

--- views/klantpaneel/FAQbekijken.php
+++ views/klantpaneel/FAQbekijken.php
@@ -4,32 +4,21 @@ $faqinfo = $output->FAQ();
 ?>
 <div class="content-panel">
 		<table class="table table-hover">
-                      echo "<table style='width:100%' border='1'>";
+                    <tr>
+                        <th>Vraag</th>
+                        <th>Beschrijving</th>
+                        <th>Oplossing</th>
+                        <th>ID Medewerker</th>
+                    </tr>
+                    
+                    <?php
 foreach ($faqinfo as $key => $value) {
     echo "<tr>";
-    echo "<td>" . $key . "</td>";
-    echo "<td>" . $value . "</td>";
+    foreach ($value as $faq)
+    {
+    echo "<td>" . $faq;
+    echo "</td>";
+    }
     echo "</tr>";
 }
-echo "<pre>";var_dump($faqinfo); echo "</pre>";
-
-
-//Open tickets checken
-//$output = new output;
-//$opentickets = $output->openTickets();
-//            echo "<table border='2'>";
-//    echo "<tr><th> Ticket ID </th>";
-//    echo "<th> Incident Type </th>";
-//    echo "<th> Probleemstelling </th>";
-//    echo "<th> Oplossing </th>";
-//    echo "<th> Bedrijfsnaam </th></tr>";
-//foreach ($opentickets as $key => $value) {
-//    echo "<tr>";
-//    foreach ($value as $ticket)
-//    {
-//        echo "<td>" . $ticket;
-//        echo "</td>";
-//    }
-//    echo "</tr>";
-//}
-//echo "<pre>";var_dump($opentickets); echo "</pre>";
+?>
