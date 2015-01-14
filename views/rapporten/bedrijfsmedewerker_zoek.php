1907e253a16be1be621efe0a585e9bde2ed24963
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-14 23:31:52 +0100

    try dis

--- views/rapporten/bedrijfsmedewerker_zoek.php
+++ views/rapporten/bedrijfsmedewerker_zoek.php
@@ -3,15 +3,15 @@ isset($_POST['zoekterm']) ? $zoekterm = filter_input(INPUT_POST, 'zoekterm') : $
 ?>
 <div class="col-lg-12">
 	<div class="form-panel">
-		<h4 class="mb"><i class="fa fa-angle-right"></i> Zoek medewerker</h4>
+		<h4 class="mb"><i class="fa fa-angle-right"></i> Zoek bedrijfsmedewerker</h4>
 		<form class="form-inline" method="POST" action="" role="form">
 			<div class="form-group">
 				<label class="sr-only" for="zoekterm">Voor of achternaam:</label>
 				<input type="text" name="zoekterm" class="form-control" id="zoekterm" placeholder="Zoekterm" value="<?= $zoekterm ?>">
 			</div>
 			<button type="submit" class="btn btn-theme">Zoek</button>
+			<small>Klik op de rij om meer informatie over de specifieke bedrijfsmedewerker te krijgen.</small>
 		</form>
-		<small>Klik op de rij om meer informatie over de specifieke bedrijfsmedewerker te krijgen.</small>
 	</div><!-- /form-panel -->
 		<?php
 		if (isset($_POST['zoekterm']))
@@ -19,33 +19,31 @@ isset($_POST['zoekterm']) ? $zoekterm = filter_input(INPUT_POST, 'zoekterm') : $
 			echo "<div class='content-panel'>";
 			$db = new db();
 			$zoekterm = filter_input(INPUT_POST, 'zoekterm');
-			$stmt = $db->link->prepare("SELECT * FROM MEDEWERKER WHERE Voornaam LIKE ? OR Achternaam LIKE ?");
+			$stmt = $db->link->prepare("SELECT * FROM BEDRIJFSMEDEWERKER WHERE Voornaam LIKE ? OR Achternaam LIKE ?");
 			$stmt->bindValue(1, "%" . $zoekterm . "%");
 			$stmt->bindValue(2, "%" . $zoekterm . "%");
 			$stmt->execute();
 			$returnArray = $stmt->fetchAll(PDO::FETCH_ASSOC);
 			echo "<table class='table table-hover'>";
 			echo "<tr>";
+			echo "<th> idBedrijf </th>";
 			echo "<th> Gebruikersnaam </th>";
-			echo "<th> E-mail </th>";
+			echo "<th> Email </th>";
 			echo "<th> Voornaam </th>";
 			echo "<th> Achternaam </th>";
 			echo "<th> Tussenvoegsel </th>";
-			echo "<th> Link </th>";
+			echo "<th> Functie </th>";
 			echo "</tr>";
 			foreach($returnArray as $result)
 			{
-				echo "<tr><a href='/rapporten/bedrijfsmedewerker/" . $id . "'>";
+				$id = $result['idBedrijfsMedewerker'];
+				echo "<tr><a href='/beheerderspaneel/bedrijfsmedewerker/edit/" . $id . "'>";
 				foreach($result as $key => $subresult){
-					if($key == "Afbeelding")
+					if($key == "idBedrijfsMedewerker")
 						continue;
-					if($key == "idMedewerker"){
-						$id = $subresult;
-					} else {
-						echo "<td>";
-						echo $subresult;
-						echo "</td>";
-					}
+					echo "<td>";
+					echo $subresult;
+					echo "</td>";
 				}
 				echo "</a></tr>";
 			}
