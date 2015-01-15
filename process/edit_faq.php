cf339ce000d35bc3a24cfb6a4469df12fa9cb496
kbosman <k.bosman35@gmail.com>
2015-01-15 14:33:32 +0100

    edit faq proces gemaakt

--- process/edit_faq.php
+++ process/edit_faq.php
@@ -4,7 +4,24 @@ $edit = new edit;
 
 if(isset($_POST['submit']) && $_POST['submit'] == 'submit'){
 	$idFAQ = $_POST['idFAQ'];
-	var_dump($edit->FAQ($idFAQ));
+if($edit->FAQ($idFAQ) == TRUE){
+
+echo'<div class="alert alert-success">
+        <a href="#" class="close" data-dismiss="alert">&times;</a>
+        <strong>Succes!</strong> De FAQ is aangepast.</div>';
+        ?><p>U word terug gestuurd naar de FAQ pagina in <span id="counter">5</span> seconden.</p>
+        <script type="text/javascript">
+        function countdown() {
+        var i = document.getElementById('counter');
+        if (parseInt(i.innerHTML)<=1) {
+        location.href = '/beheerderspaneel/faq/';
+    }
+    i.innerHTML = parseInt(i . innerHTML) - 1;
+}
+setInterval(function(){countdown();}, 1000);
+</script><?php
+}
+
 } else {
 	echo "Please post the next time!";
 }
