7a8794a25d01b3e45f6370a9b448674fc8649a23
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-15 12:01:39 +0100

    bugfix

--- views/instellingen/afbeelding.php
+++ views/instellingen/afbeelding.php
@@ -1,5 +1,5 @@
 <div class="col-lg-10 showback">
-	<form action="/process/afbeelding/" method="POST">
+	<form action="/process/afbeelding/" method="POST" enctype="multipart/form-data">
 		<div class="fileinput fileinput-new" data-provides="fileinput">
 			<div class="fileinput-new thumbnail" style="width: 200px; height: 150px;">
 				<img src="https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-512.png" />
