3ff0353358ad6812c225f2cb99a6158fc54d344c
kbosman <k.bosman35@gmail.com>
2015-01-15 14:25:14 +0100

    edit medewerker proces aangepast

--- process/edit_medewerker.php
+++ process/edit_medewerker.php
@@ -4,7 +4,22 @@ $edit = new edit;
 
 if(isset($_POST['submit']) && $_POST['submit'] == 'submit'){
 	$idMedewerker = $_POST['idMedewerker'];
-	var_dump($edit->medewerker($idMedewerker));
+	if($edit->medewerker($idMedewerker) == TRUE){
+	echo'<div class="alert alert-success">
+        <a href="#" class="close" data-dismiss="alert">&times;</a>
+        <strong>Succes!</strong> De medewerker is aangepast.</div>';
+        ?><p>U word terug gestuurd naar de medewerker pagina in <span id="counter">5</span> seconden.</p>
+        <script type="text/javascript">
+        function countdown() {
+        var i = document.getElementById('counter');
+        if (parseInt(i.innerHTML)<=1) {
+        location.href = '/process/edit/medewerker/';
+    }
+    i.innerHTML = parseInt(i . innerHTML) - 1;
+}
+setInterval(function(){countdown();}, 1000);
+</script><?php
+	}
 } else {
 	echo "Please post the next time!";
 }
