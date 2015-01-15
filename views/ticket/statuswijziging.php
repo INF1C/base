776db6d580c652df4e0f72c09c9b83d74bdbd86d
rbeikes <memyselfandi105@hotmail.com>
2015-01-15 13:25:47 +0100

    test

--- views/ticket/statuswijziging.php
+++ views/ticket/statuswijziging.php
@@ -3,9 +3,9 @@ $idTicket = $params;
 $output = new output;
 $ticketinfo = $output->ticket(5);
 
-foreach($ticketinfo as $test)
+foreach($ticketinfo as $test1 => $test)
 {
-    echo "<th>";
+    echo "<tr><td>" . $test1 . "</td>";
     foreach($test as $key)
     {
         echo "<tr>" . $key . "</tr>";
