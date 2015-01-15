f6db4ae73de21e1f442e14effb4f9b03ff680c35
kbosman <k.bosman35@gmail.com>
2015-01-15 15:51:00 +0100

    create ticket process gemaakt

--- process/create_ticket_klant.php
+++ process/create_ticket_klant.php
@@ -10,6 +10,25 @@ if(isset($_POST['submit']) && $_POST['submit'] == 'submit'){
 	$idBedrijfsMedewerker = $create->db->select(array('idBedrijfsMedewerker'), array('Gebruikersnaam' => $_SESSION['gebruikersnaam']))[0]['idBedrijfsMedewerker'];
 	$idBedrijf = $create->db->select(array('idBedrijf'), array('idBedrijfsMedewerker' => $idBedrijfsMedewerker))[0]['idBedrijf'];
 	var_dump($create->statusWijziging($ticketID, $idBedrijfsMedewerker, $idBedrijf, NULL, "Nieuw"));
+	if($create->statusWijziging($ticketID, $idBedrijfsMedewerker, $idBedrijf, NULL, "Nieuw") == TRUE){
+	
+	echo'<div class="alert alert-success">
+        <a href="#" class="close" data-dismiss="alert">&times;</a>
+        <strong>Succes!</strong> De ticket is aangemaakt.</div>';
+        ?><p>U word terug gestuurd naar de ticket indienen pagina in <span id="counter">5</span> seconden.</p>
+        <script type="text/javascript">
+        function countdown() {
+        var i = document.getElementById('counter');
+        if (parseInt(i.innerHTML)<=1) {
+        location.href = '/klantpaneel/ticket/';
+    }
+    i.innerHTML = parseInt(i . innerHTML) - 1;
+}
+setInterval(function(){countdown();}, 1000);
+</script><?php
+	
+
+	} 
 } else {
 	echo "Please post the next time!";
 }
