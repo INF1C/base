1c8dc62a753bd7c1f70a70b605f9d3360d40feea
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-15 21:00:23 +0100

    close modal

--- views/ticket/zoekbedrijfsmedewerker.php
+++ views/ticket/zoekbedrijfsmedewerker.php
@@ -31,7 +31,7 @@
 						echo "</td>";
 					}
 				}
-				echo "<td><a href='' onclick=\"$('#VeldBedrijfsMedewerker').val('" . $id . "'); return false;\">Klik hier om te bewerken</a></td>";
+				echo "<td><a href='' onclick=\"$('#VeldBedrijfsMedewerker').val('" . $id . "'); $('#bedrijfsmedewerkermodal').modal('hide'); return false;\">Selecteer</a></td>";
 				echo "</tr>";
 			}
 			echo "</table>";
