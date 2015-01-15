273649471393461ad33b1e02b1c4daa23e9a9d74
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-15 14:05:36 +0100

    edit form

--- process/create_bedrijf.php
+++ process/create_bedrijf.php
@@ -4,22 +4,22 @@ $create = new create;
 
 if(isset($_POST['submit']) && $_POST['submit'] == 'submit'){
 	var_dump($create->bedrijf());
-        if($create->bedrijf()== True){  
+    if($create->bedrijf()== True){  
         echo'<div class="alert alert-success">
-        <a href="#" class="close" data-dismiss="alert">&times;</a>
-        <strong>Succes!</strong> Het bedrijf is aangemaakt.</div>';
+            <a href="#" class="close" data-dismiss="alert">&times;</a>
+            <strong>Succes!</strong> Het bedrijf is aangemaakt.</div>';
         ?><p>U word terug gestuurd naar de bedrijfs pagina in <span id="counter">5</span> seconden.</p>
         <script type="text/javascript">
-        function countdown() {
-        var i = document.getElementById('counter');
-        if (parseInt(i.innerHTML)<=1) {
-        location.href = '/beheerderspaneel/faq/';
+            function countdown() {
+                var i = document.getElementById('counter');
+                if (parseInt(i.innerHTML)<=1) {
+                    location.href = '/beheerderspaneel/faq/';
+                }
+                i.innerHTML = parseInt(i . innerHTML) - 1;
+            }
+            setInterval(function(){countdown();}, 1000);
+        </script><?php
     }
-    i.innerHTML = parseInt(i . innerHTML) - 1;
-}
-setInterval(function(){countdown();}, 1000);
-</script><?php
-}
 } else
 {
 echo "Probeer het opnieuw alstublieft.";
