0ea1f3b5881f5a4bf387f25ab7a37c63b0a1fc12
SGiest <s.g.iest@hotmail.com>
2015-01-15 12:55:31 +0100

    testje

--- views/rapporten/open_tickets.php
+++ views/rapporten/open_tickets.php
@@ -1,6 +1,7 @@
 <div class="col-lg-12 showback">
 	<h2>Niet behandelde Tickets</h2>
 	<?php
+$result = $output->tickets(NULL, array('start' => $start, 'stop' => $stop));
 
 $output = new output;
 $opentickets = $output->NietBehandeldeTickets();
