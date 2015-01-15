7406f9c9bf668a1ae42f6273679224565cee9c7a
rbeikes <memyselfandi105@hotmail.com>
2015-01-15 13:14:14 +0100

    test

--- views/ticket/statuswijziging.php
+++ views/ticket/statuswijziging.php
@@ -1,8 +1,10 @@
 <?php
 $idTicket = $params;
+echo "<pre>"; var_dump($idTicket); echo "</pre>";
 $output = new output;
+echo "<pre>"; var_dump($output); "</pre>";
 $ticketinfo = $output->ticket($idTicket);
-
+echo "<pre>"; var_dump($ticketinfo); echo "</pre>";
 echo $ticketinfo;
 
 ?>
