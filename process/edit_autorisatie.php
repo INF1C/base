0152037ea4e87636de4fd0edc72458f59a1fbb39
kbosman <k.bosman35@gmail.com>
2015-01-15 14:55:16 +0100

    autorisatie proces done

--- process/edit_autorisatie.php
+++ process/edit_autorisatie.php
@@ -4,5 +4,25 @@ if(isset($_POST['submit']) && $_POST['submit'] == 'submit'){
 	$db->db_table = "ACCOUNT";
 	$data = array('Autorisatie' => filter_input(INPUT_POST, 'Autorisatie'));
 	$where = array('Gebruikersnaam' => filter_input(INPUT_POST, 'Gebruikersnaam'));
-	var_dump($db->update($data, $where));
+	if($db->update($data, $where) == TRUE){
+	
+	echo'<div class="alert alert-success">
+        <a href="#" class="close" data-dismiss="alert">&times;</a>
+        <strong>Succes!</strong> De autorisatie is aangepast.</div>';
+        ?><p>U word terug gestuurd naar de autorisatie pagina in <span id="counter">5</span> seconden.</p>
+        <script type="text/javascript">
+        function countdown() {
+        var i = document.getElementById('counter');
+        if (parseInt(i.innerHTML)<=1) {
+        location.href = '/instellingen/autorisatie/';
+    }
+    i.innerHTML = parseInt(i . innerHTML) - 1;
 }
+setInterval(function(){countdown();}, 1000);
+</script><?php
+	
+	}
+}else{
+	echo "Please post the next time!";
+}
+
