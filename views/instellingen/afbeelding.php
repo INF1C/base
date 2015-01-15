a393403d05e1b53220308af605561de089799aa6
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-15 19:04:19 +0100

    bug fix

--- views/instellingen/afbeelding.php
+++ views/instellingen/afbeelding.php
@@ -1,6 +1,6 @@
 <div class="col-lg-10 showback">
 	<form action="/process/afbeelding/" method="POST" enctype="multipart/form-data">
-		<div class="fileinput fileinput-new" data-provides="fileinput">
+		<div class="fileinput <?= $afbeelding != '' ? 'fileinput-new' : 'fileinput-exists' ?>" data-provides="fileinput">
 			<div class="fileinput-new thumbnail" style="width: 200px; height: 150px;">
 				<img src="https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-512.png" />
 			</div>
@@ -17,6 +17,7 @@
 				</span>
 				<a href="#" class="btn fileinput-exists" data-dismiss="fileinput">Verwijder</a>
 			</div>
+			<br>
 			<div class="input-group">
 				<button type="submit" class="btn btn-default" name="submit" value="submit">Verstuur</button>
 			</div>
