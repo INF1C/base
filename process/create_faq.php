a7ec4b81610dc77bbe37b3c245966b4e7f0334c2
martijn13795 <martijn13795@hotmail.com>
2015-01-15 12:53:29 +0100

    javascript werkend

--- process/create_faq.php
+++ process/create_faq.php
@@ -7,11 +7,11 @@ if (isset($_POST['submit']) && $_POST['submit'] == 'submit')
         echo'<div class="alert alert-success">
         <a href="#" class="close" data-dismiss="alert">&times;</a>
         <strong>Succes!</strong> Het ticket is ingevoerd in de FAQ.</div>';
-        <script type="text/javascript">
+        ?><script type="text/javascript">
         function countdown() {
         var i = document.getElementById('counter');
         if (parseInt(i.innerHTML)<=0) {
-        location.href = 'index.php';
+        location.href = '/beheerderspaneel/faq/';
     }
     i.innerHTML = parseInt(i . innerHTML) - 1;
 }
@@ -19,7 +19,7 @@ setInterval(function()
 {
     countdown();
 }, 1000);
-</script>
+</script><?php
 }
 } else
 {
