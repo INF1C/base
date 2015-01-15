da15a6f10611ac11a99b8be2495b6cd4d3bb85f0
rbeikes <memyselfandi105@hotmail.com>
2015-01-15 13:26:40 +0100

    test

--- views/ticket/statuswijziging.php
+++ views/ticket/statuswijziging.php
@@ -2,7 +2,7 @@
 $idTicket = $params;
 $output = new output;
 $ticketinfo = $output->ticket(5);
-
+var_dump($ticketinfo);
 foreach($ticketinfo as $test1 => $test)
 {
     echo "<tr><td>" . $test1 . "</td>";
