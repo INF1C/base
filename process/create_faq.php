037ea293ffd573fb292946bc61178c58cfe2c40a
martijn13795 <martijn13795@hotmail.com>
2015-01-15 12:36:34 +0100

    var dump eruit gehaald

--- process/create_faq.php
+++ process/create_faq.php
@@ -1,13 +1,13 @@
 <?php
 
 $create = new create;
-if(isset($_POST['submit']) && $_POST['submit'] == 'submit'){
-	var_dump($create->faq());
-
-        if($create->faq()== TRUE){
-            echo"<div class='alert alert-success'></div>";
-        }
-        
-} else {
-	echo "Please post the next time!";
+if (isset($_POST['submit']) && $_POST['submit'] == 'submit')
+{
+    if ($create->faq() == TRUE)
+    {
+        echo"<div class='alert alert-success'></div>";
+    }
+} else
+{
+    echo "Please post the next time!";
 }
