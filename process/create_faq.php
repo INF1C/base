1a813a069f8a312d845b039536abe332c4bb850c
martijn13795 <martijn13795@hotmail.com>
2015-01-15 12:33:16 +0100

    allert toegevoegd aan create_faq
    <div class="alert alert-success">

--- process/create_faq.php
+++ process/create_faq.php
@@ -3,6 +3,11 @@
 $create = new create;
 if(isset($_POST['submit']) && $_POST['submit'] == 'submit'){
 	var_dump($create->faq());
+
+        if($create->faq()== TRUE){
+            echo"<div class='alert alert-success'></div>";
+        }
+        
 } else {
 	echo "Please post the next time!";
 }
