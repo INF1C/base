636638ccddc1ca02f8cca894ce712e42459be4d4
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-15 14:49:10 +0100

    edit bedrijf

--- views/beheerderspaneel/bedrijf/edit.php
+++ views/beheerderspaneel/bedrijf/edit.php
@@ -16,7 +16,7 @@ if(!empty(FILTER_INPUT(INPUT_GET, 'idBedrijf'))) {
        </div>
     <div class="form-group">
         <label for="Adresgegevens">Adresgegevens:</label>
-      <textarea placeholder="Adres gegevens" class="form-control" id="Adresgegevens" name="Adresgegevens"><?= $data['Bedrijfsnaam'] ?> </textarea>
+      <textarea placeholder="Adres gegevens" class="form-control" id="Adresgegevens" name="Adresgegevens"><?= $data['Adresgegevens'] ?> </textarea>
     </div>
     <div class="form-group">
         <label for="Telefoon">Telefoon:</label>
@@ -26,14 +26,19 @@ if(!empty(FILTER_INPUT(INPUT_GET, 'idBedrijf'))) {
         <label for="Email">E-mail:</label>
         <input type="email" class="form-control" id="Email" name="Email" value="<?= $data['Email'] ?>" placeholder="E-mail" />
        </div>
-    <div class="form-group">
-      <label for="Licentie">Licentie</label>
-        <input id="Licentie" class="form-control" placeholder="Heeft u een licentie?">
-      <select name="Licentie">
-        <option value="1" <?= $data['Licentie'] == 1 ? "selected" : '' ?>>Nee</option>
-        <option value="2" <?= $data['Licentie'] == 2 ? "selected" : '' ?>>Ja</option>
-      </select>
+       <label>Licentie</label>
+    <div class="radio">
+      <label>
+        <input type="radio" name="optionsRadios" id="optionsRadios1" name="Licentie" value="2" <?= $data['Licentie'] == 2 ? "selected" : '' ?>>
+       Ja
+      </label>
     </div>
+     <div class="radio">
+    <label>
+     <input type="radio" name="optionsRadios" id="optionsRadios2" name="Licentie" value="1" <?= $data['Licentie'] == 1 ? "selected" : '' ?>>
+      Nee
+    </label>
+</div>
     <div class="form-group">
         <input type="hidden" value="<?= $data['idBedrijf'] ?>" name="idBedrijf" />
         <input type="submit" value="submit" name="submit" />
