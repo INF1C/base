acc65d845b1e043ac97f856bb24a035e897b4ec9
martijn13795 <martijn13795@hotmail.com>
2015-01-15 13:41:37 +0100

    create.bedrijfs.php error aangepast

--- process/create_bedrijf.php
+++ process/create_bedrijf.php
@@ -4,6 +4,23 @@ $create = new create;
 
 if(isset($_POST['submit']) && $_POST['submit'] == 'submit'){
 	var_dump($create->bedrijf());
-} else {
-	echo "Please post the next time!";
+        if($create->bedrijf()== True){  
+        echo'<div class="alert alert-success">
+        <a href="#" class="close" data-dismiss="alert">&times;</a>
+        <strong>Succes!</strong> Het bedrijf is aangemaakt.</div>';
+        ?><p>U word terug gestuurd naar de bedrijfs pagina in <span id="counter">10</span> seconden.</p>
+        <script type="text/javascript">
+        function countdown() {
+        var i = document.getElementById('counter');
+        if (parseInt(i.innerHTML)<=1) {
+        location.href = '/beheerderspaneel/faq/';
+    }
+    i.innerHTML = parseInt(i . innerHTML) - 1;
+}
+setInterval(function(){countdown();}, 1000);
+</script><?php
+}
+} else
+{
+echo "Probeer het opnieuw alstublieft.";
 }
