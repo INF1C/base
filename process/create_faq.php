3e7de39e6dc333c844eda480fa1ea2d3ece44dd3
martijn13795 <martijn13795@hotmail.com>
2015-01-15 12:37:45 +0100

    allert gefixed

--- process/create_faq.php
+++ process/create_faq.php
@@ -5,7 +5,10 @@ if (isset($_POST['submit']) && $_POST['submit'] == 'submit')
 {
     if ($create->faq() == TRUE)
     {
-        echo"<div class='alert alert-success'></div>";
+        echo'<div class="alert alert-success">
+    <a href="#" class="close" data-dismiss="alert">&times;</a>
+    <strong>Success!</strong> Your message has been sent successfully.
+</div>';
     }
 } else
 {
