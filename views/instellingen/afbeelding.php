4ff60a7a46252828f0803d153eb7100c6cf3c3f1
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-15 19:09:43 +0100

    wrong comparison

--- views/instellingen/afbeelding.php
+++ views/instellingen/afbeelding.php
@@ -1,6 +1,6 @@
 <div class="col-lg-10 showback">
 	<form action="/process/afbeelding/" method="POST" enctype="multipart/form-data">
-		<div class="fileinput <?= $afbeelding != '' ? 'fileinput-new' : 'fileinput-exists' ?>" data-provides="fileinput">
+		<div class="fileinput <?= $afbeelding == '' ? 'fileinput-new' : 'fileinput-exists' ?>" data-provides="fileinput">
 			<div class="fileinput-new thumbnail" style="width: 200px; height: 150px;">
 				<img src="https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-512.png" />
 			</div>
