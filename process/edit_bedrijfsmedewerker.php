05da10404f55bcba70bfb187c26b8d29501986e6
kbosman <k.bosman35@gmail.com>
2015-01-15 14:37:49 +0100

    edit bedrijfsmedewerker process gemaakt

--- process/edit_bedrijfsmedewerker.php
+++ process/edit_bedrijfsmedewerker.php
@@ -4,7 +4,27 @@ $edit = new edit;
 
 if(isset($_POST['submit']) && $_POST['submit'] == 'submit'){
 	$idBedrijfsMedewerker = $_POST['idBedrijfsMedewerker'];
-	var_dump($edit->bedrijfsMedewerker($idBedrijfsMedewerker));
+	if($edit->bedrijfsMedewerker($idBedrijfsMedewerker) == TRUE){
+	
+	echo'<div class="alert alert-success">
+        <a href="#" class="close" data-dismiss="alert">&times;</a>
+        <strong>Succes!</strong> De bedrijfsmedewerker is aangepast.</div>';
+        ?><p>U word terug gestuurd naar de bewerk pagina in <span id="counter">5</span> seconden.</p>
+        <script type="text/javascript">
+        function countdown() {
+        var i = document.getElementById('counter');
+        if (parseInt(i.innerHTML)<=1) {
+        location.href = '/beheerderspaneel/bedrijfsmedewerker/';
+    }
+    i.innerHTML = parseInt(i . innerHTML) - 1;
+}
+setInterval(function(){countdown();}, 1000);
+</script><?php
+
+	
+	
+	
+	}
 } else {
 	echo "Please post the next time!";
 }
