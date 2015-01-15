b5872c87bbc13c79398e10839cb2a440dddab5cd
martijn13795 <martijn13795@hotmail.com>
2015-01-15 13:03:58 +0100

    test432432

--- process/create_faq.php
+++ process/create_faq.php
@@ -7,18 +7,16 @@ if (isset($_POST['submit']) && $_POST['submit'] == 'submit')
         echo'<div class="alert alert-success">
         <a href="#" class="close" data-dismiss="alert">&times;</a>
         <strong>Succes!</strong> Het ticket is ingevoerd in de FAQ.</div>';
-        ?><p>U word terug gestuurd naar de FAQ pagina in <span id="counter">10<span> seconden.</p><script type="text/javascript">
+        ?><p>U word terug gestuurd naar de FAQ pagina in <span id="counter">10<span> seconden.</p>
+        <script type="text/javascript">
         function countdown() {
         var i = document.getElementById('counter');
-        if (parseInt(i.innerHTML)<=0) {
+        if (parseInt(i.innerHTML)<=1) {
         location.href = '/beheerderspaneel/faq/';
     }
     i.innerHTML = parseInt(i . innerHTML) - 1;
 }
-setInterval(function()
-{
-    countdown();
-}, 1000);
+setInterval(function(){countdown();}, 1000);
 </script><?php
 }
 } else
