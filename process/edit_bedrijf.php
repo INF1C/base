bfdfdfba48ecd706b032a24f3aed8351944ede2d
kbosman <k.bosman35@gmail.com>
2015-01-15 14:41:10 +0100

    edit bedrijf process gemaakt

--- process/edit_bedrijf.php
+++ process/edit_bedrijf.php
@@ -5,6 +5,26 @@ $edit = new edit;
 if(isset($_POST['submit']) && $_POST['submit'] == 'submit'){
 	$idBedrijf = $_POST['idBedrijf'];
 	var_dump($edit->bedrijf($idBedrijf));
+	if($edit->bedrijf($idBedrijf) == TRUE){
+	
+	echo'<div class="alert alert-success">
+        <a href="#" class="close" data-dismiss="alert">&times;</a>
+        <strong>Succes!</strong> Het bedrijf is aangepast.</div>';
+        ?><p>U word terug gestuurd naar de bedrijf pagina in <span id="counter">5</span> seconden.</p>
+        <script type="text/javascript">
+        function countdown() {
+        var i = document.getElementById('counter');
+        if (parseInt(i.innerHTML)<=1) {
+        location.href = '/beheerderspaneel/bedrijf/';
+    }
+    i.innerHTML = parseInt(i . innerHTML) - 1;
+}
+setInterval(function(){countdown();}, 1000);
+</script><?php
+
+	
+	
+	}
 } else {
 	echo "Please post the next time!";
 }
