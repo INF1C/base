4595457f2c8b18a8728b7e4ff0c18f2f06af5719
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-15 10:00:28 +0100

    test

--- views/instellingen/afbeelding.php
+++ views/instellingen/afbeelding.php
@@ -4,9 +4,9 @@
 			<div class="fileinput-new thumbnail" style="width: 200px; height: 150px;">
 				<img src="http://www.placehold.it/200x150/EFEFEF/AAAAAA&text=no+image" />
 			</div>
-			<div class="fileinput-preview fileinput-exists thumbnail" style="max-width: 200px; max-height: 150px; line-height: 20px;">
+			<!--<div class="fileinput-preview fileinput-exists thumbnail" style="max-width: 200px; max-height: 150px; line-height: 20px;">
 				<img src="/images/example.png">
-			</div>
+			</div> -->
 			<div>
 				<span class="btn btn-file"><span class="fileinput-new" data-trigger="fileinput">Selecteer afbeelding</span><span class="fileinput-exists" data-trigger="fileinput">Verander</span><input type="file" name="avatar-image" /></span>
 				<a href="#" class="btn fileinput-exists" data-dismiss="fileinput">Verwijder</a>
