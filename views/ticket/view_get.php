c01eef63d3813782b731a7d884e92c6e7929c946
rbeikes <memyselfandi105@hotmail.com>
2015-01-16 10:16:15 +0100

    test

--- views/ticket/view_get.php
+++ views/ticket/view_get.php
@@ -70,6 +70,7 @@ reset($status);
     <?php
     //  echo "<td><a href='/ticket/statuswijziging/'>Nieuwe status</a></td>";
     echo "<td><a href='/ticket/statuswijziging/" . $idTicket . "'>Nieuwe status</a></td>";
+    echo "<td><a href='/ticket/editticket/'>Ticket bewerken</a></td>";
     ?>
 </div>
 
