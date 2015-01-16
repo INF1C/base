664fe8156b34c009b68bdddb619e0e8b4e4bfec6
martijn13795 <martijn13795@hotmail.com>
2015-01-16 13:25:18 +0100

    tabeletjes weg gehaald

--- views/root.php
+++ views/root.php
@@ -5,68 +5,7 @@ MAIN CONTENT
 <div class="row">
     <div class="col-lg-12">
         <div class="row mt">
-            <div class="col-md-4 col-sm-4 mb">
-                <div class="white-panel pn">
-                    <div class="white-header">
-                        <h5>Panel 1</h5>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-6 col-xs-6 goleft">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 col-sm-4 mb">
-                <div class="white-panel pn">
-                    <div class="white-header">
-                        <h5>Panel 2</h5>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-6 col-xs-6 goleft">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 col-sm-4 mb">
-                <div class="white-panel pn">
-                    <div class="white-header">
-                        <h5>Panel 3</h5>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-6 col-xs-6 goleft">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-4 col-sm-4 mb">
-                <div class="white-panel pn">
-                    <div class="white-header">
-                        <h5>Panel 4</h5>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-6 col-xs-6 goleft">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 col-sm-4 mb">
-                <div class="white-panel pn">
-                    <div class="white-header">
-                        <h5>Panel 5</h5>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-6 col-xs-6 goleft">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 col-sm-4 mb">
-                <div class="white-panel pn">
-                    <div class="white-header">
-                        <h5>Panel 6</h5>
-                    </div>
+            
                     <div class="row">
                         <div class="col-sm-6 col-xs-6 goleft">
                         </div>
