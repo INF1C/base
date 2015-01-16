206b6ec547c0d9ac398542e88ec48363d3dc29a0
martijn13795 <martijn13795@hotmail.com>
2015-01-16 15:18:28 +0100

    w weg bij wehelp

--- views/root.php
+++ views/root.php
@@ -1,2 +1,2 @@
-<img src="/base/templates/subCss/wehelp.png" class="img-responsive center-block" style="padding-top: 70px">
+<img src="/base/templates/subCss/ehelp.png" class="img-responsive center-block" style="padding-top: 70px">
    
