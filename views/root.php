73c0f0395dd023adac6ea3da163f7e52297d726a
martijn13795 <martijn13795@hotmail.com>
2015-01-16 13:46:14 +0100

    padding 70

--- views/root.php
+++ views/root.php
@@ -1,2 +1,2 @@
-<img src="/base/templates/subCss/wehelp.png" class="img-responsive center-block" style="padding-top: 150px">
+<img src="/base/templates/subCss/wehelp.png" class="img-responsive center-block" style="padding-top: 70px">
    
