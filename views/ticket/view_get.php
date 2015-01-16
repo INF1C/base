d010fba203e451868cd04e256ac505a70c2f2425
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-16 16:06:35 +0100

    typo

--- views/ticket/view_get.php
+++ views/ticket/view_get.php
@@ -30,12 +30,12 @@ reset($status);
             <td><?= $ticket['Oplossing'] ?></td>
         </tr>
     </table>
-    ?php if($_SESSION['autorisatie'] != 'Bedrijfsmedewerker') { ?>
+    <?php if($_SESSION['autorisatie'] != 'Bedrijfsmedewerker') { ?>
             <a href='/ticket/editticket/<?= $idTicket ?>'>Ticket bewerken</a>
-        <?php }
-        if($_SESSION['autorisatie'] === 'Admin') { ?>
-            <a class="pull-right" href='/beheerderspaneel/verwijder/ticket/<?= $idTicket ?>'>Ticket verwijderen</a>
-        <?php } ?>
+    <?php }
+    if($_SESSION['autorisatie'] === 'Admin') { ?>
+        <a class="pull-right" href='/beheerderspaneel/verwijder/ticket/<?= $idTicket ?>'>Ticket verwijderen</a>
+    <?php } ?>
 </div>
 <div class="col-lg-3 showback pull-right">
     <?php
