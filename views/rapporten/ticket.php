979f1325351b6e8978c499964df085466b7d97ad
rbeikes <memyselfandi105@hotmail.com>
2015-01-16 14:48:08 +0100

    test

--- views/rapporten/ticket.php
+++ views/rapporten/ticket.php
@@ -3,8 +3,8 @@
     <h4 class="mb"><i class="fa fa-angle-right"></i> Voer het ticket ID in</h4>
     <form class="form-inline" method="POST" action="/ticket/" role="form">
         <div class="form-group">
-            <label class="sr-only" for="zoekterm" required>TicketID:</label>
-            <input type="text" name="idTicket" class="form-control" id="zoekterm" placeholder="Ticket ID">
+            <label class="sr-only" for="zoekterm">TicketID:</label>
+            <input type="text" name="idTicket" class="form-control" id="zoekterm" placeholder="Ticket ID" required>
         </div>
         <button type="submit" class="btn btn-theme">Ga</button>
     </form>
