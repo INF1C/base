fe20938cafcbf0756c64fc51f818822772704926
martijn13795 <martijn13795@hotmail.com>
2015-01-16 13:35:36 +0100

    max-... test

--- views/root.php
+++ views/root.php
@@ -1,5 +1,5 @@
 <section class="wrapper main-chart">
-    <img src="/base/templates/subCss/wehelp.png">
+    <img src="/base/templates/subCss/wehelp.png" style="max-height: 100%; max-width: 100%">
 </section>
 
    
