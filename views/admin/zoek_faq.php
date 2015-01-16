598ad588ca6806bef478afb27b0131cba6408c42
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-01-16 14:18:21 +0100

    verwijder faq

---
+++ views/admin/zoek_faq.php
@@ -0,0 +1,32 @@
+<div class="col-lg-12 showback">
+    <h2>FAQ</h2>
+
+    <?php
+    $output = new output;
+    $faqinfo = $output->FAQ();
+    echo "<table class='table table-hover'>";
+    echo "<tr>";
+    echo "<th> Vraag </th>";
+    echo "<th> Beschrijving </th>";
+    echo "<th> Oplossing </ht>";
+    echo "<th> Auteur </th>";
+    echo "</tr>";
+    $output->db->db_table = "MEDEWERKER";
+    foreach ($faqinfo as $value) {
+        echo "<tr>";
+        foreach ($value as $key => $subvalue) {
+            if ($key == "idFAQ") {
+                $idFAQ = $subvalue;
+            } elseif ($key == "idMedewerker") {
+                $idMedewerker = $subvalue;
+            } else {
+                echo "<td>" . $subvalue . "</td>";
+            }
+        }
+        echo "<td>" . implode(' ', $output->db->select(array('Voornaam', 'Achternaam'), array('idMedewerker' => $idMedewerker))[0]) . "</td>";
+        echo "<td><a href='/beheerderspaneel/faq/edit/" . $idFAQ . "'>Bewerk</a></td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    ?>
+</div>
