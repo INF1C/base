4d3de422ad703ed1cf4bbb8120a5e1cee8bde1db
rbeikes <memyselfandi105@hotmail.com>
2015-01-16 14:44:53 +0100

    test

--- views/rapporten/ticket.php
+++ views/rapporten/ticket.php
@@ -1,7 +1,7 @@
 <div class="col-lg-12 showback">
     <h1>Ticket</h1>
     <h4 class="mb"><i class="fa fa-angle-right"></i> Voer het ticket ID in</h4>
-    <form class="form-inline" method="POST" action="/ticket/" role="form">
+    <form class="form-inline" method="POST" action="/ticket/" role="form" required>
         <div class="form-group">
             <label class="sr-only" for="zoekterm">TicketID:</label>
             <input type="text" name="idTicket" class="form-control" id="zoekterm" placeholder="Ticket ID">
